7d16bb57535bf54e2204aed2257bf1d7f72fb48d
He, Jiehui <he_jiehui@163.com>
2013-09-02 11:26:50 +0800

    integrate QueryExecutor

--- das/src/main/java/com/ctrip/sysdev/das/service/DalServiceImpl.java
+++ das/src/main/java/com/ctrip/sysdev/das/service/DalServiceImpl.java
@@ -9,12 +9,14 @@ import com.ctrip.sysdev.das.domain.Response;
 import com.ctrip.sysdev.das.domain.enums.ResultTypeEnum;
 import com.ctrip.sysdev.das.exception.SerDeException;
 import com.ctrip.sysdev.das.serde.MsgPackSerDe;
+import com.ctrip.sysdev.das.worker.QueryExecutor;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
 public class DalServiceImpl extends AbstractDalService implements DalService {
 
 	private MsgPackSerDe<Response> msgPackSerDe;
+	private QueryExecutor executor = new QueryExecutor();
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Inject
@@ -31,7 +33,7 @@ public class DalServiceImpl extends AbstractDalService implements DalService {
 		ByteBuf buf = Unpooled.buffer(10);
 		buf.writeInt(26);
 		buf.writeShort(1);
-		Response response = new Response();
+		Response response = executor.execute(getDataSourceWrapper(), request.getMessage());
 		response.setTaskid(request.getTaskid());
 		response.setResultType(ResultTypeEnum.CUD);
 		response.setAffectRowCount(1);

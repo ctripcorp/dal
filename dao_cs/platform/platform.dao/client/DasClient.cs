c037a42079d5465616dc712bf3fff8da998ed8ab
gawu <gawu@ctrip.com>
2013-11-19 12:15:46 +0800

    fix pb bug

--- dao_cs/platform/platform.dao/client/DasClient.cs
+++ dao_cs/platform/platform.dao/client/DasClient.cs
@@ -37,7 +37,8 @@ namespace platform.dao.client
 
         public void Init()
         {
-            socketPool = new SocketPool("172.16.155.184", ServicePort);
+            socketPool = new SocketPool("192.168.83.132", ServicePort);
+            //socketPool = new SocketPool("127.0.0.1", ServicePort);
         }
 
         /// <summary>

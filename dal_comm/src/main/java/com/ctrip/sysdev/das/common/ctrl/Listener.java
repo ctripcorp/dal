df4bf6f847dac2e0f77c5321ceaa9c3450034c9b
He, Jiehui <he_jiehui@163.com>
2014-01-16 13:44:15 +0800

    revised

--- dal_comm/src/main/java/com/ctrip/sysdev/das/common/ctrl/Listener.java
+++
@@ -1,6 +0,0 @@
-package com.ctrip.sysdev.das.common.ctrl;
-
-public interface Listener {
-	void onMessage(Message msg);
-	void onError(Error error);
-}
--- das_console/src/main/java/com/ctrip/platform/dasconsole/resource/ConfigureResource.java
+++ das_console/src/main/java/com/ctrip/platform/dasconsole/resource/ConfigureResource.java
@@ -1,5 +1,9 @@
 package com.ctrip.platform.dasconsole.resource;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import javax.annotation.Resource;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
@@ -11,9 +15,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Resource
 @Path("configure/snapshot")
 @Singleton
-public class ConfigureResource extends DalBaseResource {
+public class ConfigureResource extends DalBaseResource implements Runnable {
+	private ScheduledExecutorService reader;
+	// Sync every 5 minutes
+	private static int PERIOD = 60 *1000 * 5;
+	
 	private ObjectMapper mapper = new ObjectMapper();
+	
 	private String latestConfig;
+	
+	public ConfigureResource() {
+		super();
+		initZk();
+		buildLatest();
+	}
 	
 	@GET
 	public String get() {
@@ -35,5 +50,28 @@ public class ConfigureResource extends DalBaseResource {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private void initZk() {
+		getZk().
+	}
+	
+	private void initTimer() {
+		reader = Executors.newSingleThreadScheduledExecutor();
+		reader.scheduleAtFixedRate(this, PERIOD, PERIOD, TimeUnit.SECONDS);
+		
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {reader.shutdown();}
+		});
+	}
+	
+	private void configureChanged() {
+		// TODO shall we refresh by different entity?
+		buildLatest();
+	}
+	
+	@Override
+	public void run() {
+		buildLatest();
 	}
 }

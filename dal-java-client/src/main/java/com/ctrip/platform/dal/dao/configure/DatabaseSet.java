789d31f143b06cf65bdde2a09ef97ce74920bef0
He, Jiehui <he_jiehui@163.com>
2014-03-14 10:50:33 +0800

    fix bug

--- dal-java-client/src/main/java/com/ctrip/platform/dal/dao/configure/DatabaseSet.java
+++ dal-java-client/src/main/java/com/ctrip/platform/dal/dao/configure/DatabaseSet.java
@@ -9,6 +9,10 @@ import java.util.Set;
 import com.ctrip.platform.dal.dao.strategy.DalShardStrategy;
 
 public class DatabaseSet {
+	private static final String CLASS = "class";
+	private static final String ENTRY_SEPARATOR = ";";
+	private static final String KEY_VALUE_SEPARATOR = "=";
+	
 	private String name;
 	private String provider;
 	private String shardStrategy;
@@ -17,12 +21,16 @@ public class DatabaseSet {
 	// Key is shard id, value is all database under in this shard
 	private Map<String, List<DataBase>> databaseByShard = new HashMap<String, List<DataBase>>();
 
-	public DatabaseSet(String name, String provider, String shardStrategy, Map<String, DataBase> databases) {
+	public DatabaseSet(String name, String provider, String shardStrategy, Map<String, DataBase> databases) throws Exception {
 		this.name = name;
 		this.provider = provider;
 		this.shardStrategy = shardStrategy;
 		this.databases = databases;
-		
+		initShards();
+		initStrategy();
+	}
+	
+	private void initShards() throws Exception {
 		// Init map by shard
 		for(DataBase db: databases.values()) {
 			List<DataBase> dbList = databaseByShard.get(db.getSharding());
@@ -32,6 +40,23 @@ public class DatabaseSet {
 			}
 			dbList.add(db);
 		}
+	}
+	
+	private void initStrategy() throws Exception {
+		if(shardStrategy == null || shardStrategy.length() == 0)
+			return;
+		
+		String[] values = shardStrategy.split(ENTRY_SEPARATOR);
+		String[] strategyDef = values[0].split(KEY_VALUE_SEPARATOR);
+		
+		if(strategyDef[0].equals(CLASS))
+			strategy = (DalShardStrategy)Class.forName(strategyDef[1]).newInstance();
+		Map<String, String> settings = new HashMap<String, String>();
+		for(int i = 1; i < values.length; i++) {
+			String[] entry = values[i].split(KEY_VALUE_SEPARATOR);
+			settings.put(entry[0], entry[1]);
+		}
+		strategy.initialize(settings);
 	}
 
 	public String getName() {

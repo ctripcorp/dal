d6fdd65cd9753cdbe04c5fa0cd9cb97a26a1cfcc
wcyuan <wcyuan@ctrip.com>
2014-04-15 13:53:56 +0800

    ADD logging

--- dal-dao-gen/src/main/java/com/ctrip/platform/dal/daogen/utils/CommonUtils.java
+++ dal-dao-gen/src/main/java/com/ctrip/platform/dal/daogen/utils/CommonUtils.java
@@ -1,9 +1,41 @@
 package com.ctrip.platform.dal.daogen.utils;
 
+import org.apache.log4j.Logger;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class CommonUtils {
+	private static ObjectMapper objectMap;
+	private static Logger log;
 	
+	static{
+		objectMap = new ObjectMapper();
+		log = Logger.getLogger(CommonUtils.class);
+	}
 	public static String normalizeVariable(String variable){
 		return variable.replaceAll("[^A-Za-z0-9()\\[\\]]", "");
 	}
 
+	public static int tryParse(String val, int defaultValue)
+	{
+		int v = defaultValue;
+		try{
+			v = Integer.parseInt(val);
+		}
+		catch(Exception e) { }
+		
+		return v;
+	}
+	
+	public static String toJson(Object o)
+	{
+		String res = "";
+		try {
+			res = objectMap.writeValueAsString(o);
+		} catch (JsonProcessingException e) {
+			log.error(e);
+		}
+		return res;
+	}
 }

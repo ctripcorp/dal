5787841b2fa2dc2ee029a78e399e7364d3cb8cf9
xgz <gzxia@ctrip.com>
2014-05-08 17:18:28 +0800

    add image.

--- dal-dao-gen/src/main/webapp/static/js/welcome.js
+++ dal-dao-gen/src/main/webapp/static/js/welcome.js
@@ -2,6 +2,10 @@
     $("#welcome-ver-list").click(function(event){
         $("li[class='todo-done']").removeClass("todo-done");
         $(event.target).addClass("todo-done");
+        $("div[class='vjs-poster']").html(" ").append("<img class=\"vjs-poster\" src=\"/static/Flat-UI-master/images/video/poster.jpg\" tabindex=\"-1\">");
+    });
+    $(window).load(function(){
+        $("div[class='vjs-poster']").html(" ").append("<img class=\"vjs-poster\" src=\"/static/Flat-UI-master/images/video/poster.jpg\" tabindex=\"-1\">");
     });
 
 })();

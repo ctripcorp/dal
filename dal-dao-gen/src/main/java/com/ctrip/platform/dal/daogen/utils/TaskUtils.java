db26716ffd9ee70c788e1c716871ba0ee4c04f3b
gawu <gawu@ctrip.com>
2014-05-20 14:17:40 +0800

    fix gen bug with

--- dal-dao-gen/src/main/java/com/ctrip/platform/dal/daogen/utils/TaskUtils.java
+++ dal-dao-gen/src/main/java/com/ctrip/platform/dal/daogen/utils/TaskUtils.java
@@ -7,13 +7,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 
 public class TaskUtils {
-	public static final int BATCH_SIZE = 100;
+	public static final int BATCH_SIZE = 50;
 	
 	public static <T> List<Future<T>> invokeBatch(ExecutorService service, 
 			List<? extends Callable<T>> tasks) throws InterruptedException{
 		List<Future<T>> result = new ArrayList<Future<T>>();
 		int loop = tasks.size();
-		for(int i = 0; i < loop; i += loop){
+		for(int i = 0; i < loop; i += BATCH_SIZE){
 			result.addAll(service.invokeAll(
 					tasks.subList(i, Math.min(loop, i + BATCH_SIZE))));
 		}

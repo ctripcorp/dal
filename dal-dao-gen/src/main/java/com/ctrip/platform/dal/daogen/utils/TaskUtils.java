bdb9d40d785fa64f672b103fcfb28d643580bac1
xgz <gzxia@ctrip.com>
2014-05-20 14:14:28 +0800

    fix TaskUtils bug.

--- dal-dao-gen/src/main/java/com/ctrip/platform/dal/daogen/utils/TaskUtils.java
+++ dal-dao-gen/src/main/java/com/ctrip/platform/dal/daogen/utils/TaskUtils.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 
 public class TaskUtils {
-	public static final int BATCH_SIZE = 100;
+	public static final int BATCH_SIZE = 50;
 	
 	public static <T> List<Future<T>> invokeBatch(ExecutorService service, 
 			List<? extends Callable<T>> tasks) throws InterruptedException{

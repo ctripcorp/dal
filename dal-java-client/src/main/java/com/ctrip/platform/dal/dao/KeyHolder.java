33f2fb00c56b10bf815b7aa0396bb6c814090152
He, Jiehui <he_jiehui@163.com>
2014-05-23 16:31:52 +0800

    add getKey(index)

--- dal-java-client/src/main/java/com/ctrip/platform/dal/dao/KeyHolder.java
+++ dal-java-client/src/main/java/com/ctrip/platform/dal/dao/KeyHolder.java
@@ -10,34 +10,36 @@ import java.util.Map;
 public class KeyHolder {
 	private final List<Map<String, Object>> keyList = new LinkedList<Map<String, Object>>();;
 
+	public int size() {
+		return keyList.size();
+	}
+	
 	/**
-	 * Get the generated Id. The type is one of the Number.
+	 * Get the generated Id. The type is of Number.
 	 * @return null if no key found, or the id in number
 	 * @throws SQLException if there is more than one generated key or the conversion is failed.
 	 */
 	public Number getKey() throws SQLException {
-		if (this.keyList.size() == 0) {
-			return null;
-		}
-		if (this.keyList.size() > 1 || this.keyList.get(0).size() > 1) {
+		if (this.keyList.size() != 1) {
 			throw new SQLException(
-					"The getKey method should only be used when a single key is returned.  " +
-					"The current key entry contains multiple keys: " + this.keyList);
+					"Non or More than one generated keys are returned: " + keyList);
 		}
-		Iterator<Object> keyIter = this.keyList.get(0).values().iterator();
-		if (keyIter.hasNext()) {
-			Object key = keyIter.next();
-			if (!(key instanceof Number)) {
-				throw new SQLException(
-						"The generated key is not of a supported numeric type. " +
-						"Unable to cast [" + (key != null ? key.getClass().getName() : null) +
-						"] to [" + Number.class.getName() + "]");
-			}
-			return (Number) key;
-		}
-		else {
-			throw new SQLException("Unable to retrieve the generated key. " +
-					"Check that the table has an identity column enabled.");
+		return getKey(0);
+	}
+
+	/**
+	 * Get the generated Id for given index. The type is of Number.
+	 * @return key in number format
+	 * @throws SQLException if there is more than one generated key or the conversion is failed.
+	 */
+	public Number getKey(int index) throws SQLException {
+		try {
+			if(keyList.get(index).size() != 1)
+				throw new SQLException("Non or More than one entries found for the generated key: " + keyList.get(index));
+			
+			return (Number)keyList.get(index).values().iterator().next();
+		} catch (Throwable e) {
+			throw new SQLException("Can not convert generated key to number.", e);
 		}
 	}
 
@@ -47,13 +49,10 @@ public class KeyHolder {
 	 * @throws SQLException
 	 */
 	public Map<String, Object> getKeys() throws SQLException {
-		if (this.keyList.size() == 0) {
-			return null;
-		}
-		if (this.keyList.size() > 1)
+		if (this.keyList.size() != 1) {
 			throw new SQLException(
-					"The getKeys method should only be used when keys for a single row are returned.  " +
-					"The current key list contains keys for multiple rows: " + this.keyList);
+					"Non or More than one generated keys are returned: " + keyList);
+		}
 		return this.keyList.get(0);
 	}
 
@@ -72,10 +71,6 @@ public class KeyHolder {
 	 */
 	public List<Number> getIdList() throws SQLException {
 		List<Number> idList = new ArrayList<Number>();
-		
-		if (this.keyList.size() == 0) {
-			return null;
-		}
 		
 		try {
 			for(Map<String, Object> key: keyList) {

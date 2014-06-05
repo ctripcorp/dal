07ddadc70866834929beaf54ca1c7c019bf715bc
He, Jiehui <he_jiehui@163.com>
2014-06-05 10:43:07 +0800

    add helper test to suit

--- dal-tester/src/main/java/com/ctrip/platform/dal/tester/AllTest.java
+++ dal-tester/src/main/java/com/ctrip/platform/dal/tester/AllTest.java
@@ -6,6 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
+	com.ctrip.platform.dal.dao.dialet.test.MySqlHelperTest.class,
 	com.ctrip.platform.dal.dao.unittests.AllTest.class,
 	com.ctrip.platform.dal.parser.AllTest.class,
 	com.ctrip.platform.dal.tester.client.AllTest.class

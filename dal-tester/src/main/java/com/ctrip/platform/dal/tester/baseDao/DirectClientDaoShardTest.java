ce4a3a6a26031dbacea2c2fe6c63b37db078e7aa
He, Jiehui <he_jiehui@163.com>
2014-06-16 18:20:27 +0800

    revise test

--- dal-tester/src/main/java/com/ctrip/platform/dal/tester/baseDao/DirectClientDaoShardTest.java
+++ dal-tester/src/main/java/com/ctrip/platform/dal/tester/baseDao/DirectClientDaoShardTest.java
@@ -1,7 +1,6 @@
 package com.ctrip.platform.dal.tester.baseDao;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -23,17 +22,19 @@ import com.ctrip.platform.dal.dao.DalHintEnum;
 import com.ctrip.platform.dal.dao.DalHints;
 import com.ctrip.platform.dal.dao.DalResultSetExtractor;
 import com.ctrip.platform.dal.dao.StatementParameters;
-import com.ctrip.platform.dal.dao.logging.DalEventEnum;
+import com.ctrip.platform.dal.dao.helper.DalScalarExtractor;
 
 public class DirectClientDaoShardTest {
 	private final static String DATABASE_NAME = "dao_test";
+	private final static String DATABASE_NAME_SQLSVR = "dao_test_sqlsvr";
+	private final static String DATABASE_NAME_MYSQL = "dao_test_mysql";
 	
 	private final static String TABLE_NAME = "dal_client_test";
 	
-	private final static String DROP_TABLE_SQL = "DROP TABLE IF EXISTS " + TABLE_NAME;
+	private final static String DROP_TABLE_SQL_MYSQL = "DROP TABLE IF EXISTS " + TABLE_NAME;
 	
 	//Create the the table
-	private final static String CREATE_TABLE_SQL = "CREATE TABLE " + TABLE_NAME +"("
+	private final static String CREATE_TABLE_SQL_MYSQL = "CREATE TABLE " + TABLE_NAME +"("
 			+ "id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, "
 			+ "quantity int,"
 			+ "type smallint, "
@@ -41,24 +42,26 @@ public class DirectClientDaoShardTest {
 			+ "last_changed timestamp default CURRENT_TIMESTAMP)";
 	
 	
-	private final static String DROP_TABLE_SQL_SQL_SVR = "IF EXISTS ("
+	private final static String DROP_TABLE_SQL_SQLSVR = "IF EXISTS ("
 			+ "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES "
 			+ "WHERE TABLE_NAME = '"+ TABLE_NAME + "') "
 			+ "DROP TABLE  "+ TABLE_NAME;
 	
 	//Create the the table
-	private final static String CREATE_TABLE_SQL_SQL_SVR = "CREATE TABLE " + TABLE_NAME +"("
+	private final static String CREATE_TABLE_SQL_SQLSVR = "CREATE TABLE " + TABLE_NAME +"("
 			+ "Id int NOT NULL IDENTITY(1,1) PRIMARY KEY, "
 			+ "quantity int,type smallint, "
 			+ "address varchar(64) not null,"
 			+ "last_changed datetime default getdate())";
 	
-	private static DalClient client = null;
-
+	private static DalClient clientSqlSvr;
+	private static DalClient clientMySql;
+	
 	static {
 		try {
 			DalClientFactory.initClientFactory("/DalMult.config");
-			client = DalClientFactory.getClient(DATABASE_NAME);
+			clientSqlSvr = DalClientFactory.getClient(DATABASE_NAME_SQLSVR);
+			clientMySql = DalClientFactory.getClient(DATABASE_NAME_MYSQL);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -67,15 +70,31 @@ public class DirectClientDaoShardTest {
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		DalHints hints = new DalHints();
-		String[] sqls = new String[] { DROP_TABLE_SQL, CREATE_TABLE_SQL};
-		client.batchUpdate(sqls, hints);
+		String[] sqls = new String[] { DROP_TABLE_SQL_MYSQL, CREATE_TABLE_SQL_MYSQL};
+		clientMySql.batchUpdate(sqls, hints);
+		
+		// For SQL server
+		hints = new DalHints();
+		StatementParameters parameters = new StatementParameters();
+		sqls = new String[] { DROP_TABLE_SQL_SQLSVR, CREATE_TABLE_SQL_SQLSVR};
+		for (int i = 0; i < sqls.length; i++) {
+			clientSqlSvr.update(sqls[i], parameters, hints);
+		}	
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		DalHints hints = new DalHints();
-		String[] sqls = new String[] { DROP_TABLE_SQL};
-		client.batchUpdate(sqls, hints);
+		String[] sqls = new String[] { DROP_TABLE_SQL_MYSQL};
+		clientMySql.batchUpdate(sqls, hints);
+		
+		//For Sql Server
+		hints = new DalHints();
+		StatementParameters parameters = new StatementParameters();
+		sqls = new String[] { DROP_TABLE_SQL_SQLSVR};
+		for (int i = 0; i < sqls.length; i++) {
+			clientSqlSvr.update(sqls[i], parameters, hints);
+		}
 	}
 
 	@Before
@@ -88,8 +107,21 @@ public class DirectClientDaoShardTest {
 						+ " VALUES(2, 11, 1, 'BJ INFO', NULL)",
 				"INSERT INTO " + TABLE_NAME
 						+ " VALUES(3, 12, 2, 'SZ INFO', NULL)" };
-		int[] counts = client.batchUpdate(insertSqls, hints);
+		int[] counts = clientMySql.batchUpdate(insertSqls, hints);
 		assertArrayEquals(new int[] { 1, 1, 1 }, counts);
+		
+		//For Sql Server
+		hints = new DalHints();
+		insertSqls = new String[] {
+				"SET IDENTITY_INSERT "+ TABLE_NAME +" ON",
+				"INSERT INTO " + TABLE_NAME + "(Id, quantity,type,address)"
+						+ " VALUES(4, 10, 1, 'SH INFO')",
+				"INSERT INTO " + TABLE_NAME + "(Id, quantity,type,address)"
+						+ " VALUES(5, 11, 1, 'BJ INFO')",
+				"INSERT INTO " + TABLE_NAME + "(Id, quantity,type,address)"
+						+ " VALUES(6, 12, 2, 'SZ INFO')",
+				"SET IDENTITY_INSERT "+ TABLE_NAME +" OFF"};
+		clientSqlSvr.batchUpdate(insertSqls, hints);
 	}
 
 	@After
@@ -98,134 +130,97 @@ public class DirectClientDaoShardTest {
 		StatementParameters parameters = new StatementParameters();
 		DalHints hints = new DalHints();
 		try {
-			client.update(sql, parameters, hints);
+			clientMySql.update(sql, parameters, hints);
 		} catch (SQLException e) {
+			e.printStackTrace();
+			fail();
+		}
+
+		sql = "DELETE FROM " + TABLE_NAME;
+		parameters = new StatementParameters();
+		hints = new DalHints();
+		try {
+			clientSqlSvr.update(sql, parameters, hints);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void test() {
+		try {
+			DalClient client = DalClientFactory.getClient(DATABASE_NAME);
+			
+			String sql = "select id from " + TABLE_NAME;
+			
+			StatementParameters parameters = new StatementParameters();
+			DalHints hints = new DalHints();
+			Map<String, Integer> colValues = new HashMap<String, Integer>();
+			colValues.put("id", 0);
+			hints.set(DalHintEnum.shardColValues, colValues);
+
+			Integer o = (Integer)client.query(sql, parameters, hints, new DalScalarExtractor());
+			assertNotNull(o);
+			
+			hints = new DalHints();
+			colValues = new HashMap<String, Integer>();
+			colValues.put("id", 1);
+			hints.masterOnly();
+			hints.set(DalHintEnum.shardColValues, colValues);
+
+			o = (Integer)client.query(sql, parameters, hints, new DalScalarExtractor());
+			assertNotNull(o);
+		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
 		}
 	}
 
 	@Test
-	public static void test() {
-		try {
-			DalClient client = DalClientFactory.getClient("AbacusDB_INSERT_1");
-			
-			String sql = "select * from AbacusAddInfoLog";
-			
-			StatementParameters parameters = new StatementParameters();
-			DalHints hints = new DalHints();
-			Map<String, Integer> colValues = new HashMap<String, Integer>();
-			colValues.put("user_id", 0);
-			hints.set(DalHintEnum.shardColValues, colValues);
-
-			client.query(sql, parameters, hints, new DalResultSetExtractor<Object>() {
-				@Override
-				public Object extract(ResultSet rs) throws SQLException {
-					while(rs.next()){
-						rs.getObject(1);
-					}
-					return null;
-				}
-				
-			});
-			
-			hints = new DalHints();
-			colValues = new HashMap<String, Integer>();
-			colValues.put("user_id", 2);
-			hints.masterOnly();
-			hints.set(DalHintEnum.shardColValues, colValues);
-
-			client.query(sql, parameters, hints, new DalResultSetExtractor<Object>() {
-				@Override
-				public Object extract(ResultSet rs) throws SQLException {
-					while(rs.next()){
-						rs.getObject(1);
-					}
-					return null;
-				}
-				
-			});
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Test
-	public static void test2() {
-		try {
-			DalClient client = DalClientFactory.getClient("AbacusDB_INSERT_1");
-			StatementParameters parameters = new StatementParameters();
-			DalHints hints = new DalHints();
-			//String delete = "update AbacusAddInfoLog set PNR='dafas' where id = 100";
-			String select = "select PNR from AbacusAddInfoLog where LOGID = 100";
-			String update = "update AbacusAddInfoLog set PNR='dafas11' where LOGID = 100";
-			String restore = "update AbacusAddInfoLog set PNR='dafas' where LOGID = 100";
-			
-			hints = new DalHints();
-			Map<String, Integer> colValues = new HashMap<String, Integer>();
-			colValues.put("user_id", 0);
-			hints.set(DalHintEnum.shardColValues, colValues);
-
-			client.update(update, parameters, hints);
-			
-			client.query(select, parameters, hints, new DalResultSetExtractor<Object>() {
-				@Override
-				public Object extract(ResultSet rs) throws SQLException {
-					while(rs.next()){
-						System.out.println(rs.getObject(1));
-					}
-					return null;
-				}
-				
-			});
-			
-
-			client.update(restore, parameters, hints);
-			
-			client.query(select, parameters, hints, new DalResultSetExtractor<Object>() {
-				@Override
-				public Object extract(ResultSet rs) throws SQLException {
-					while(rs.next()){
-						System.out.println(rs.getObject(1));
-					}
-					return null;
-				}
-				
-			});
-						
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private static Integer typeTest(Object objVal) {
-		if(objVal == null || objVal instanceof Integer)
-			return (Integer)objVal;
-		
-		return Integer.valueOf(((Number)objVal).intValue());
-
-	}
-
-	public static void main(String[] args) {
-		ILog logger = LogManager.getLogger("DAL Java Client");
-		logger.debug("test");
-		System.out.print(LogConfig.getAppID());
-		try {
-			DalClientFactory.initClientFactory("/DalMult.config");
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
-		
-//		test();
-//		test2();
-
-		System.out.println(typeTest(null));
-		System.out.println(typeTest(Integer.valueOf(-1)));
-		System.out.println(typeTest(new Short((short)-1)));
-		System.out.println(typeTest(new Byte((byte)-1)));
-		
-		System.exit(0);
-	}
+	public void test2() {
+//		try {
+//			DalClient client = DalClientFactory.getClient("AbacusDB_INSERT_1");
+//			StatementParameters parameters = new StatementParameters();
+//			DalHints hints = new DalHints();
+//			//String delete = "update AbacusAddInfoLog set PNR='dafas' where id = 100";
+//			String select = "select PNR from AbacusAddInfoLog where LOGID = 100";
+//			String update = "update AbacusAddInfoLog set PNR='dafas11' where LOGID = 100";
+//			String restore = "update AbacusAddInfoLog set PNR='dafas' where LOGID = 100";
+//			
+//			hints = new DalHints();
+//			Map<String, Integer> colValues = new HashMap<String, Integer>();
+//			colValues.put("user_id", 0);
+//			hints.set(DalHintEnum.shardColValues, colValues);
+//
+//			client.update(update, parameters, hints);
+//			
+//			client.query(select, parameters, hints, new DalResultSetExtractor<Object>() {
+//				@Override
+//				public Object extract(ResultSet rs) throws SQLException {
+//					while(rs.next()){
+//						System.out.println(rs.getObject(1));
+//					}
+//					return null;
+//				}
+//				
+//			});
+//			
+//
+//			client.update(restore, parameters, hints);
+//			
+//			client.query(select, parameters, hints, new DalResultSetExtractor<Object>() {
+//				@Override
+//				public Object extract(ResultSet rs) throws SQLException {
+//					while(rs.next()){
+//						System.out.println(rs.getObject(1));
+//					}
+//					return null;
+//				}
+//				
+//			});
+//						
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+	}	
 }

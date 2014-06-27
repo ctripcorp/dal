b1219ec9268308d5cd2a031255eb120c6320272e
wcyuan <wcyuan@ctrip.com>
2014-06-27 13:55:03 +0800

    mysql concurrent unit test

--- dal-tester/src/main/java/com/ctrip/platform/dal/dao/unittests/DalConcurrentMysqlTest.java
+++ dal-tester/src/main/java/com/ctrip/platform/dal/dao/unittests/DalConcurrentMysqlTest.java
@@ -5,38 +5,38 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.sql.Types;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import junit.framework.Assert;
 
-import org.apache.commons.lang.StringUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.ctrip.platform.dal.dao.DalClient;
 import com.ctrip.platform.dal.dao.DalClientFactory;
+import com.ctrip.platform.dal.dao.DalCommand;
 import com.ctrip.platform.dal.dao.DalHints;
-import com.ctrip.platform.dal.dao.DalRowMapper;
+import com.ctrip.platform.dal.dao.DalParser;
+import com.ctrip.platform.dal.dao.DalTableDao;
+import com.ctrip.platform.dal.dao.KeyHolder;
 import com.ctrip.platform.dal.dao.StatementParameters;
-import com.ctrip.platform.dal.dao.helper.DalRowMapperExtractor;
+import com.ctrip.platform.dal.dao.helper.DalScalarExtractor;
+
 
 public class DalConcurrentMysqlTest {
 	
 	private final static String DATABASE_NAME = "dao_test";
-	
 	private final static String TABLE_NAME = "dal_client_test";
-	private final static String SP_I_NAME = "dal_client_test_i";
-	private final static String SP_D_NAME="dal_client_test_d";
-	private final static String SP_U_NAME = "dal_client_test_u";
 	
+	private static List<Number> generateIds;
+	private static Random random = new Random();
 	private final static int INSERT_COUNT = 1000;
-	private final static Random random = new Random();
 	
 	private final static String DROP_TABLE_SQL = "DROP TABLE IF EXISTS " + TABLE_NAME;
-	
-	//Create the the table
 	private final static String CREATE_TABLE_SQL = "CREATE TABLE dal_client_test("
 			+ "id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, "
 			+ "quantity int,"
@@ -44,154 +44,215 @@ public class DalConcurrentMysqlTest {
 			+ "address VARCHAR(64) not null, "
 			+ "last_changed timestamp default CURRENT_TIMESTAMP)";
 	
-	//Only has normal parameters
-	private static final String CREATE_I_SP_SQL = "CREATE PROCEDURE dal_client_test_i("
-			+ "dal_id int,"
-			+ "quantity int,"
-			+ "type smallint,"
-			+ "address VARCHAR(64)) "
-			+ "BEGIN INSERT INTO dal_client_test"
-			+ "(id, quantity, type, address) "
-			+ "VALUES(dal_id, quantity, type, address);"
-			+ "SELECT ROW_COUNT() AS result;"
-			+ "END";
-	//Has out parameters store procedure
-	private static final String CREATE_D_SP_SQL = "CREATE PROCEDURE dal_client_test_d("
-			+ "dal_id int,"
-			+ "out count int)"
-			+ "BEGIN DELETE FROM dal_client_test WHERE id=dal_id;"
-			+ "SELECT ROW_COUNT() AS result;"
-			+ "SELECT COUNT(*) INTO count from dal_client_test;"
-			+ "END";
-	//Has in-out parameters store procedure
-	private static final String CREATE_U_SP_SQL = "CREATE PROCEDURE dal_client_test_u("
-			+ "dal_id int,"
-			+ "quantity int,"
-			+ "type smallint,"
-			+ "INOUT address VARCHAR(64))"
-			+ "BEGIN UPDATE dal_client_test "
-			+ "SET quantity = quantity, type=type, address=address "
-			+ "WHERE id=dal_id;"
-			+ "SELECT ROW_COUNT() AS result;"
-			+ "END";
-	
-	private static final String DROP_I_SP_SQL = "DROP PROCEDURE  IF  EXISTS dal_client_test_i";
-	private static final String DROP_D_SP_SQL = "DROP PROCEDURE  IF  EXISTS dal_client_test_d";
-	private static final String DROP_U_SP_SQL = "DROP PROCEDURE  IF  EXISTS dal_client_test_u";
-	
 	private static DalClient client = null;
-	private static ClientTestDalRowMapper mapper = null;
-
+	private static DalTableDao<ClientTestModel> dao = null;
+	private static DalParser<ClientTestModel> parser = null;
 	static {
 		try {
 			DalClientFactory.initClientFactory();
 			client = DalClientFactory.getClient(DATABASE_NAME);
-			mapper = new ClientTestDalRowMapper();
+			parser = new ClientTestDalParser();
+			dao = new DalTableDao<ClientTestModel>(parser);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		DalHints hints = new DalHints();
-		String[] sqls = new String[] { DROP_TABLE_SQL, CREATE_TABLE_SQL, 
-				DROP_I_SP_SQL, CREATE_I_SP_SQL, 
-				DROP_D_SP_SQL, CREATE_D_SP_SQL,
-				DROP_U_SP_SQL, CREATE_U_SP_SQL};
+		String[] sqls = new String[] { DROP_TABLE_SQL, CREATE_TABLE_SQL};
 		client.batchUpdate(sqls, hints);
 		
-		String[] insertSqls = new String[INSERT_COUNT];
+		ClientTestModel[] models = new ClientTestModel[INSERT_COUNT];
 		for (int i = 0; i < INSERT_COUNT; i++) {
-			insertSqls[i] = String.format("INSERT INTO " + TABLE_NAME
-						+ " VALUES(%s, %s, %s, '%s', NULL)", i+1, i%100, i%10, "SH INFO " + i);
+			ClientTestModel model = new ClientTestModel();
+			model.setAddress("SH-" + i);
+			model.setLastChanged(new Timestamp(System.currentTimeMillis()));
+			model.setQuantity(i%10);
+			model.setType((short)(i%3));
+			
+			models[i] = model;
 		}
-		client.batchUpdate(insertSqls, hints);
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		DalHints hints = new DalHints();
-		String[] sqls = new String[] { DROP_TABLE_SQL, DROP_I_SP_SQL,
-				DROP_D_SP_SQL, DROP_U_SP_SQL};
-		client.batchUpdate(sqls, hints);
+		
+		KeyHolder holder = new KeyHolder();
+		
+		int count = dao.insert(hints,holder, models);
+		System.out.println("The insert ClientTestModel count: " + count);	
+		generateIds = holder.getIdList();
+		System.out.println("Ids: " + generateIds.size());
+		
+		Assert.assertEquals(INSERT_COUNT, generateIds.size());
 	}
 
 	@Test
-	public void testQueryAll() throws SQLException {
-		String querySql = "SELECT * FROM " + TABLE_NAME;
-		StatementParameters parameters = new StatementParameters();
-		ClientTestDalRowMapper mapper = new ClientTestDalRowMapper();
-		DalHints hints = new DalHints();
-		List<ClientTestModel> res = client.query(querySql, parameters, hints,
-				new DalRowMapperExtractor<ClientTestModel>(mapper));
-		Assert.assertTrue(null != res && res.size() == INSERT_COUNT);
+	public void testQueryById() throws SQLException{
+		int maxId = random.nextInt(INSERT_COUNT);
+		List<ClientTestModel> models = dao.query("Id > " + generateIds.get(maxId).intValue(), new StatementParameters(), new DalHints());
+		System.out.println(models.size());
 	}
 	
 	@Test
+	public void testCount() throws SQLException{
+		String sql = "SELECT count(1) from " + TABLE_NAME;
+		Number count = (Number)client.query(sql, new StatementParameters(), new DalHints(), new DalScalarExtractor());
+		System.out.println("count = " + count);
+		Assert.assertEquals(INSERT_COUNT, count.intValue());
+	}
+	
+	@Test
+	public void testQueryByIdRange() throws SQLException{
+		int maxId = random.nextInt(INSERT_COUNT);
+		int minId = random.nextInt(maxId);
+		List<ClientTestModel> models = dao.query("Id > " + generateIds.get(minId).intValue() + " and Id <= " + generateIds.get(maxId).intValue(), 
+				new StatementParameters(), new DalHints());
+		System.out.println(models.size());
+		Assert.assertEquals(generateIds.get(maxId).intValue() - generateIds.get(minId).intValue(), models.size());
+	}
+
+	@Test
 	public void testUpdate() throws SQLException{
-		int id = random.nextInt(1000);
-		List<ClientTestModel> models = this.queryModelsByIds(id);
-		ClientTestModel model = null;
-		if(models.size() > 0)
-			model = models.get(0);
-		else{
-			System.out.println(id);
-			return;
-		}
-		String updateSql = String.format(
-				"UPDATE %s SET address=? WHERE id=?", TABLE_NAME);
-		StatementParameters parameters = new StatementParameters();
-		if(model.getAddress().contains("SH"))
-			parameters.set(1, Types.VARCHAR, model.getAddress().replace("SH", "BJ"));
+		int id = random.nextInt(INSERT_COUNT);
+		String updateSql = "UPDATE " + TABLE_NAME + " SET address = ? WHERE id = ?";
+		StatementParameters param = new StatementParameters();
+		if(id % 2 == 0)
+			param.set(1, Types.VARCHAR, "BJ-" + id);
 		else
-			parameters.set(1, Types.VARCHAR, model.getAddress().replace("BJ", "SH"));
-		parameters.set(2, Types.INTEGER,  model.getId());
-		DalHints hints = new DalHints();
-		int count = client.update(updateSql, parameters, hints);
+			param.set(1, Types.VARCHAR, "SH-" + id);
+		param.set(2, Types.INTEGER, id);
+		int count = dao.update(updateSql, param, new DalHints());
+		System.out.println("update souccess: " + id);
 		Assert.assertEquals(1, count);
 	}
-
-	/**
-	 * Get the models all in dal_client_test by specified IDs
-	 * 
-	 * @param ids
-	 * @return The list of ClientTestModel
-	 */
-	private List<ClientTestModel> queryModelsByIds(int... ids) {
-		List<ClientTestModel> models = new ArrayList<ClientTestModel>();
-		String querySql = "";
-		if (null != ids && ids.length > 0) {
-			Integer[] idds = new Integer[ids.length];
-			for (int i = 0; i < idds.length; i++) {
-				idds[i] = ids[i];
-			}
-			querySql = "SELECT * FROM %s WHERE id in(%s)";
-			String inClause = StringUtils.join(idds, ",");
-			querySql = String.format(querySql, TABLE_NAME, inClause);
-		} else {
-			querySql = "SELECT * FROM " + TABLE_NAME;
-		}
-		StatementParameters parameters = new StatementParameters();
-		DalHints hints = new DalHints();
-		try {
-			models = client.query(querySql, parameters, hints,
-					new DalRowMapperExtractor<ClientTestModel>(mapper));
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return models;
+	
+	@Test
+	public void testExecute() throws SQLException{
+		List<DalCommand> commands = new ArrayList<DalCommand>();
+		commands.add(new DalCommand(){
+			@Override
+			public boolean execute(DalClient client) throws SQLException {
+				String sql = "INSERT INTO " + TABLE_NAME + "(quantity,type,address)" + " VALUES(10, 1, 'SH INFO')";
+				int ret = client.update(sql, new StatementParameters(), new DalHints());
+				if(ret > 0) {
+					System.out.println("insert success.");
+				} else{
+					System.out.println("insert failed.");
+				}
+				return ret > 0;
+			}});
+		commands.add(new DalCommand(){
+			@Override
+			public boolean execute(DalClient client) throws SQLException {
+				String sql = "DELETE FROM " + TABLE_NAME + " WHERE address = '" + "SH INFO'";
+				int ret = client.update(sql, new StatementParameters(), new DalHints());
+				if(ret > 0){
+					System.out.println("delete success");
+				}else{
+					System.out.println("delete failed");
+				}
+				return ret > 0;
+			}});
+		client.execute(commands, new DalHints());
+		
+		String sql = "SELECT count(1) from " + TABLE_NAME;
+		Number count = (Number)client.query(sql, new StatementParameters(), new DalHints(), new DalScalarExtractor());
+		Assert.assertEquals(INSERT_COUNT, count.intValue());
 	}
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		DalHints hints = new DalHints();
+		String[] sqls = new String[] { DROP_TABLE_SQL };
+		client.batchUpdate(sqls, hints);
+	}
+	
+	
+	
+	private static class ClientTestDalParser implements DalParser<ClientTestModel>{
+		private static final String databaseName="dao_test";
+		private static final String tableName= "dal_client_test";
+		private static final String[] columnNames = new String[]{
+			"id","quantity","type","address","last_changed"
+		};
+		private static final String[] primaryKeyNames = new String[]{"id"};
+		private static final int[] columnTypes = new int[]{
+			Types.INTEGER, Types.INTEGER, Types.SMALLINT, Types.VARCHAR, Types.TIMESTAMP
+		};
+		@Override
+		public ClientTestModel map(ResultSet rs, int rowNum)
+				throws SQLException {
+			ClientTestModel model = new ClientTestModel();
+			model.setId(rs.getInt(1));
+			model.setQuantity(rs.getInt(2));
+			model.setType(rs.getShort(3));
+			model.setAddress(rs.getString(4));
+			model.setLastChanged(rs.getTimestamp(5));
+			return model;
+		}
 
+		@Override
+		public String getDatabaseName() {
+			return databaseName;
+		}
+
+		@Override
+		public String getTableName() {
+			return tableName;
+		}
+
+		@Override
+		public String[] getColumnNames() {
+			return columnNames;
+		}
+
+		@Override
+		public String[] getPrimaryKeyNames() {
+			return primaryKeyNames;
+		}
+
+		@Override
+		public int[] getColumnTypes() {
+			return columnTypes;
+		}
+
+		@Override
+		public boolean isAutoIncrement() {
+			return true;
+		}
+
+		@Override
+		public Number getIdentityValue(ClientTestModel pojo) {
+			return pojo.getId();
+		}
+
+		@Override
+		public Map<String, ?> getPrimaryKeys(ClientTestModel pojo) {
+			Map<String, Object> keys = new LinkedHashMap<String, Object>();
+			keys.put("id", pojo.getId());
+			return keys;
+		}
+
+		@Override
+		public Map<String, ?> getFields(ClientTestModel pojo) {
+			Map<String, Object> map = new LinkedHashMap<String, Object>();
+			map.put("id", pojo.getId());
+			map.put("quantity", pojo.getQuantity());
+			map.put("type", pojo.getType());
+			map.put("address", pojo.getAddress());
+			map.put("last_changed", pojo.getLastChanged());
+			return map;
+		}
+		
+	}
+	
 	private static class ClientTestModel {
-		private int id;
-		private int quantity;
-		private short type;
+		private Integer id;
+		private Integer quantity;
+		private Short type;
 		private String address;
 		private Timestamp lastChanged;
 
-		public int getId() {
+		public Integer getId() {
 			return id;
 		}
 
@@ -199,7 +260,7 @@ public class DalConcurrentMysqlTest {
 			this.id = id;
 		}
 
-		public int getQuantity() {
+		public Integer getQuantity() {
 			return quantity;
 		}
 
@@ -207,7 +268,7 @@ public class DalConcurrentMysqlTest {
 			this.quantity = quantity;
 		}
 
-		public short getType() {
+		public Short getType() {
 			return type;
 		}
 
@@ -229,22 +290,6 @@ public class DalConcurrentMysqlTest {
 
 		public void setLastChanged(Timestamp lastChanged) {
 			this.lastChanged = lastChanged;
-		}
-	}
-
-	private static class ClientTestDalRowMapper implements
-			DalRowMapper<ClientTestModel> {
-
-		@Override
-		public ClientTestModel map(ResultSet rs, int rowNum)
-				throws SQLException {
-			ClientTestModel model = new ClientTestModel();
-			model.setId(rs.getInt(1));
-			model.setQuantity(rs.getInt(2));
-			model.setType(rs.getShort(3));
-			model.setAddress(rs.getString(4));
-			model.setLastChanged(rs.getTimestamp(5));
-			return model;
 		}
 	}
 }

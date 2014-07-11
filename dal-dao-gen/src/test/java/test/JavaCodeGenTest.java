e92fc0b5fd751dfee7b66998834a8f53f4bd4ce8
xgz <gzxia@ctrip.com>
2014-07-11 18:01:12 +0800

    add xunit.

---
+++ dal-dao-gen/src/test/java/test/JavaCodeGenTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.util.Map;
+
+import com.ctrip.platform.dal.daogen.entity.Progress;
+import com.ctrip.platform.dal.daogen.generator.java.JavaCodeGenContext;
+import com.xross.tools.xunit.Processor;
+import com.xross.tools.xunit.XrossFactory;
+
+public class JavaCodeGenTest {
+	public static void main(String[] args) {
+//		Processor javaCodeGen = null;
+//		try {
+//			javaCodeGen = XrossFactory.createFromXML("code_gen.xunit").getProcessor("Java Code Generator");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			System.exit(0);
+//		}
+//		int projectId = 1234;
+//		boolean regenerate = true;
+//		Progress progress = null;
+//		Map<String, ?> hints = null;
+//		
+//		JavaCodeGenContext ctx = new JavaCodeGenContext(
+//				projectId, 
+//				regenerate,
+//				progress, 
+//				hints);
+//		javaCodeGen.process(ctx);
+	}
+}

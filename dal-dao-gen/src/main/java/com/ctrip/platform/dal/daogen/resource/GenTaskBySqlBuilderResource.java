315950aca16d85d34182f3537644379986c86abf
xgz <gzxia@ctrip.com>
2014-08-06 18:37:23 +0800

    add build sql validation.

--- dal-dao-gen/src/main/java/com/ctrip/platform/dal/daogen/resource/GenTaskBySqlBuilderResource.java
+++ dal-dao-gen/src/main/java/com/ctrip/platform/dal/daogen/resource/GenTaskBySqlBuilderResource.java
@@ -2,6 +2,9 @@
 package com.ctrip.platform.dal.daogen.resource;
 
 import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.inject.Singleton;
@@ -20,9 +23,13 @@ import com.ctrip.platform.dal.daogen.entity.DatabaseSetEntry;
 import com.ctrip.platform.dal.daogen.entity.GenTaskBySqlBuilder;
 import com.ctrip.platform.dal.daogen.entity.LoginUser;
 import com.ctrip.platform.dal.daogen.enums.CurrentLanguage;
+import com.ctrip.platform.dal.daogen.host.AbstractParameterHost;
+import com.ctrip.platform.dal.daogen.host.java.JavaParameterHost;
 import com.ctrip.platform.dal.daogen.utils.DbUtils;
+import com.ctrip.platform.dal.daogen.utils.SQLValidation;
 import com.ctrip.platform.dal.daogen.utils.SpringBeanGetter;
 import com.ctrip.platform.dal.daogen.utils.SqlBuilder;
+import com.ctrip.platform.dal.daogen.utils.SQLValidation.Validation;
 
 /**
  * 构建SQL（生成的代码绑定到模板）
@@ -60,6 +67,8 @@ public class GenTaskBySqlBuilderResource {
 			@FormParam("pagination") boolean pagination,
 			@FormParam("orderby") String orderby) {
 		
+		Status status = Status.OK;
+		
 		GenTaskBySqlBuilder task = new GenTaskBySqlBuilder();
 
 		if (action.equalsIgnoreCase("delete")) {
@@ -68,6 +77,10 @@ public class GenTaskBySqlBuilderResource {
 				return Status.ERROR;
 			}	
 		}else{
+			Status temp = validateSQL(set_name, table_name, crud_type, condition, sql_content, pagination);
+			if(!Status.OK.getCode().equals(temp.getCode())){
+				return temp;
+			}
 			String userNo = AssertionHolder.getAssertion().getPrincipal()
 					.getAttributes().get("employee").toString();
 			LoginUser user = SpringBeanGetter.getDaoOfLoginUser().getUserByNo(userNo);
@@ -92,7 +105,7 @@ public class GenTaskBySqlBuilderResource {
 				task.setVersion(daoBySqlBuilder.getVersionById(id));
 				task.setSql_content(sql_content);
 				if (0 >= daoBySqlBuilder.updateTask(task)) {
-					Status status = Status.ERROR;
+					status = Status.ERROR;
 					status.setInfo("更新出错，数据是否合法？或者已经有同名方法？");
 					return status;
 				}
@@ -101,14 +114,14 @@ public class GenTaskBySqlBuilderResource {
 				task.setVersion(1);
 				task.setSql_content(sql_content);
 				if (0 >= daoBySqlBuilder.insertTask(task)) {
-					Status status = Status.ERROR;
+					status = Status.ERROR;
 					status.setInfo("新增出错，数据是否合法？或者已经有同名方法？");
 					return status;
 				}
 			}
 		}
 
-		return Status.OK;
+		return status;
 	}
 	
 	@POST
@@ -151,6 +164,78 @@ public class GenTaskBySqlBuilderResource {
 		}
 		return status;
 	}
-
+	
+	private Status validateSQL(String set_name, String table_name,
+			String crud_type, String condition, String sql_content,
+			boolean pagination) {
+		Status status = Status.OK;
+		if(condition!=null && !condition.isEmpty()){
+			String []parameters = condition.split(";");
+			int paramCount = 0;
+			for(String param : parameters){
+				if("6".equals(param.split(",")[1])){
+					paramCount = paramCount + 2;
+				}else{
+					paramCount++;
+				}
+			}
+			
+			int []paramsTypes = new int[paramCount];
+			
+			DatabaseSetEntry databaseSetEntry = SpringBeanGetter.getDaoOfDatabaseSet().getMasterDatabaseSetEntryByDatabaseSetName(set_name);
+			String dbName = databaseSetEntry.getConnectionString();
+			List<AbstractParameterHost> paramsHost = DbUtils.getAllColumnNames(dbName, table_name, CurrentLanguage.Java);
+			Map<String,Integer> map = new HashMap<String,Integer>();
+			for(int i=0;i<paramsHost.size();i++){
+				JavaParameterHost paramHost = (JavaParameterHost) paramsHost.get(i);
+				map.put(paramHost.getAlias().toLowerCase(), paramHost.getSqlType());
+			}
+			
+			int index = 0;
+			for(String param : parameters){
+				String columnName = param.split(",")[0].toLowerCase();
+				if("6".equals(param.split(",")[1])){
+					paramsTypes[index++] = map.get(columnName);
+					paramsTypes[index++] = map.get(columnName);
+				}else{
+					paramsTypes[index++] = map.get(columnName);
+				}
+			}
+			
+			String tempSQL = sql_content;
+			Validation validResult;
+			try {
+				if (pagination && "select".equals(crud_type)) {
+					tempSQL = SqlBuilder.pagingQuerySql(tempSQL, DbUtils
+							.getDatabaseCategory(databaseSetEntry.getConnectionString()),
+							CurrentLanguage.Java);
+					tempSQL = String.format(tempSQL, 1, 2);
+				}
+				
+				validResult = null;
+				if ("select".equalsIgnoreCase(crud_type)) {
+					validResult = SQLValidation.queryValidate(dbName,
+							tempSQL, paramsTypes);
+				} else {
+					validResult = SQLValidation.updateValidate(dbName,
+							tempSQL, paramsTypes);
+				}
+			} catch (Exception e) {
+				status = Status.ERROR;
+				status.setInfo("验证SQL出错!"+e.getMessage());
+				return status;
+			}
+			if(validResult.isPassed()){
+				status.setInfo(validResult.getMessage());
+			}else{
+				status = Status.ERROR;
+				status.setInfo(validResult.getMessage());
+				return status;
+			}
+			
+		}
+		return status;
+	}
+	
 
 }

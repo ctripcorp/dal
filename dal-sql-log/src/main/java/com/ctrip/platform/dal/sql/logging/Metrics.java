1f85dd3bea2d897653d65b08092c68d8960baa59
wcyuan <wcyuan@ctrip.com>
2014-10-13 16:29:54 +0800

    。

---
+++ dal-sql-log/src/main/java/com/ctrip/platform/dal/sql/logging/Metrics.java
@@ -0,0 +1,36 @@
+package com.ctrip.platform.dal.sql.logging;
+
+import com.ctrip.framework.clogging.agent.metrics.IMetric;
+import com.ctrip.framework.clogging.agent.metrics.MetricManager;
+import com.ctrip.platform.dal.logging.markdown.MarkDownInfo;
+import com.ctrip.platform.dal.logging.sql.OptInfo;
+import com.ctrip.platform.dal.logging.sql.SQLInfo;
+
+public class Metrics {
+	private static IMetric metric = MetricManager.getMetricer();
+	public static long ticksPerMillisecond = 10000;
+	private static String SUCCESS = "success";
+	private static String FAIL = "fail";
+	
+	public static void report(MarkDownInfo info, long value){
+		metric.log(MarkDownInfo.KEY, value, info.toTag());
+	}
+	
+	public static void success(LogEntry entry, long duration) {
+		report(entry.getDatabaseName(), entry.isMaster() ? "Master" : "Slave", entry.getEvent().name());
+		SQLInfo info = new SQLInfo(entry.getDao(), entry.getMethod(), entry.getSqlSize(), SUCCESS);
+		metric.log(SQLInfo.COST, duration * ticksPerMillisecond, info.toTag());
+		metric.log(SQLInfo.COUNT, 1, info.toTag());
+	}
+	
+	public static void fail(LogEntry entry, long duration) {
+		SQLInfo info = new SQLInfo(entry.getDao(), entry.getMethod(), entry.getSqlSize(), FAIL);
+		metric.log(SQLInfo.COST, duration * ticksPerMillisecond, info.toTag());
+		metric.log(SQLInfo.COUNT, 1, info.toTag());
+	}
+	
+	private static void report(String databaseSet, String databaseType,String operationType){
+		OptInfo info = new OptInfo(databaseSet, databaseType, operationType);
+		metric.log(OptInfo.KEY, 1, info.toTag());
+	}
+}

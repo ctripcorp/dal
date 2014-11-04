39c24c8e2d6d8903352fa046b232917765c176ba
xgz <gzxia@ctrip.com>
2014-11-04 18:24:29 +0800

    add user group dao.

---
+++ dal-dao-gen/src/main/java/com/ctrip/platform/dal/daogen/dao/UserGroupDao.java
@@ -0,0 +1,105 @@
+package com.ctrip.platform.dal.daogen.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.ctrip.platform.dal.daogen.entity.UserGroup;
+
+public class UserGroupDao {
+
+	private JdbcTemplate jdbcTemplate;
+
+	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	public List<UserGroup> getAllUserGroup() {
+		return this.jdbcTemplate.query(
+				"select id, user_id, group_id from user_group",
+				new RowMapper<UserGroup>() {
+					public UserGroup mapRow(ResultSet rs, int rowNum)
+							throws SQLException {
+						return UserGroup.visitRow(rs);
+					}
+				});
+	}
+	
+	public List<UserGroup> getUserGroupById(Integer id){
+		return this.jdbcTemplate.query(
+				"select id, user_id, group_id from user_group where id = ?",
+				new Object[]{id},
+				new RowMapper<UserGroup>() {
+					public UserGroup mapRow(ResultSet rs, int rowNum)
+							throws SQLException {
+						return UserGroup.visitRow(rs);
+					}
+				});
+	}
+	
+	public List<UserGroup> getUserGroupByUserId(Integer userId){
+		return this.jdbcTemplate.query(
+				"select id, user_id, group_id from user_group where user_id = ?",
+				new Object[]{userId},
+				new RowMapper<UserGroup>() {
+					public UserGroup mapRow(ResultSet rs, int rowNum)
+							throws SQLException {
+						return UserGroup.visitRow(rs);
+					}
+				});
+	}
+	
+	public List<UserGroup> getUserGroupByGroupId(Integer groupId){
+		return this.jdbcTemplate.query(
+				"select id, user_id, group_id from user_group where group_id = ?",
+				new Object[]{groupId},
+				new RowMapper<UserGroup>() {
+					public UserGroup mapRow(ResultSet rs, int rowNum)
+							throws SQLException {
+						return UserGroup.visitRow(rs);
+					}
+				});
+	}
+	
+	public int insertUserGroup(UserGroup ug) {
+		return this.jdbcTemplate.update(
+				"insert into user_group(id, user_id, group_id) value(?,?,?)",
+				ug.getId(), 
+				ug.getUser_id(), 
+				ug.getGroup_id());
+	}
+	
+	public int insertUserGroup(Integer user_id, Integer group_id) {
+		return this.jdbcTemplate.update(
+				"insert into user_group(user_id, group_id) value(?,?)",
+				user_id, 
+				group_id);
+	}
+	
+	public int deleteUserFromGroup(Integer user_id, Integer group_id){
+		try {
+			return this.jdbcTemplate.update("delete from user_group where user_id=? and group_id=?",
+					user_id,
+					group_id);
+		} catch (DataAccessException ex) {
+			ex.printStackTrace();
+			return -1;
+		}
+	}
+	
+	public int deleteUserFromGroup(Integer ugId){
+		try {
+			return this.jdbcTemplate.update("delete from user_group where id=?", ugId);
+		} catch (DataAccessException ex) {
+			ex.printStackTrace();
+			return -1;
+		}
+	}
+
+}
---
+++ dal-dao-gen/src/main/java/com/ctrip/platform/dal/daogen/entity/UserGroup.java
@@ -0,0 +1,39 @@
+package com.ctrip.platform.dal.daogen.entity;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserGroup {
+
+	private int id;
+	private int user_id;
+	private int group_id;
+	
+	public static UserGroup visitRow(ResultSet rs) throws SQLException {
+		UserGroup ug = new UserGroup();
+		ug.setId(rs.getInt("id"));
+		ug.setUser_id(rs.getInt("user_id"));
+		ug.setGroup_id(rs.getInt("group_id"));
+		return ug;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public int getGroup_id() {
+		return group_id;
+	}
+	public void setGroup_id(int group_id) {
+		this.group_id = group_id;
+	}
+	
+}

dd8d28b0d2c8795642cb035f4de4da1f71d053c6
xgz夏光智 <gzxia@ctrip.com>
2015-02-25 18:30:18 +0800

    revise datasource param value.

--- datasource-locator/src/main/java/com/ctrip/datasource/configure/DatabaseConfigParser.java
+++ datasource-locator/src/main/java/com/ctrip/datasource/configure/DatabaseConfigParser.java
@@ -2,9 +2,6 @@ package com.ctrip.datasource.configure;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;

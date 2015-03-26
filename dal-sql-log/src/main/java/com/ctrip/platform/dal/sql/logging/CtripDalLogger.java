884b72af9a6e985f94b8d952c5f04c730d4e5d29
He, Jiehui <he_jiehui@163.com>
2015-03-26 17:19:23 +0800

    add missings

---
+++ dal-sql-log/src/main/java/com/ctrip/platform/dal/sql/logging/CtripDalLogger.java
@@ -0,0 +1,89 @@
+package com.ctrip.platform.dal.sql.logging;
+
+import java.util.Map;
+
+import com.ctrip.framework.clogging.domain.thrift.LogLevel;
+import com.ctrip.platform.dal.dao.client.DalLogger;
+import com.ctrip.platform.dal.dao.client.LogEntry;
+import com.ctrip.platform.dal.dao.markdown.MarkDownInfo;
+import com.ctrip.platform.dal.dao.markdown.MarkupInfo;
+
+public class CtripDalLogger implements DalLogger {
+	private static final String ENABLED = "switch";
+	private static final String SAMPLING = "sampling";
+	private static final String ENCRYPT = "encrypt";
+	
+	@Override
+	public void initLogger(Map<String, String> settings) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void info(String msg) {
+		DalCLogger.log(LogLevel.INFO, msg);
+	}
+
+	@Override
+	public void warn(String msg) {
+		DalCLogger.log(LogLevel.WARN, msg);
+	}
+
+	@Override
+	public void error(String msg, Throwable e) {
+		DalCLogger.error(msg, e);	
+	}
+
+	@Override
+	public void getConnectionFailed(String dbName, Throwable e) {
+		DalCLogger.logGetConnectionFailed(dbName, e);
+	}
+
+	@Override
+	public LogEntry createLogEntry() {
+		return new CtripLogEntry();
+	}
+
+	@Override
+	public void start(LogEntry entry) {
+		DalCatLogger.start((CtripLogEntry)entry);
+	}
+	
+	@Override
+	public void success(LogEntry entry, int count) {
+		try {
+			DalCLogger.success((CtripLogEntry)entry, count);
+			MetricsLogger.success((CtripLogEntry)entry, entry.getDuration());
+			DalCatLogger.catTransactionSuccess((CtripLogEntry)entry);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void fail(LogEntry entry, Throwable e) {
+		try {
+			DalCLogger.fail((CtripLogEntry)entry, e);
+			MetricsLogger.fail((CtripLogEntry)entry, entry.getDuration());
+			DalCatLogger.catTransactionFailed((CtripLogEntry)entry, e);
+		} catch (Throwable ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	@Override
+	public void markdown(MarkDownInfo markdown) {
+		Metrics.report(markdown);
+		
+	}
+
+	@Override
+	public void markup(MarkupInfo markup) {
+		Metrics.report(markup);
+		
+	}
+
+	@Override
+	public void shutdown() {
+		// TODO Auto-generated method stub
+	}
+}
---
+++ dal-sql-log/src/main/java/com/ctrip/platform/dal/sql/logging/DalCatLogger.java
@@ -0,0 +1,56 @@
+package com.ctrip.platform.dal.sql.logging;
+
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.ctrip.framework.clogging.domain.thrift.LogLevel;
+import com.ctrip.platform.dal.catlog.CatInfo;
+import com.ctrip.platform.dal.dao.client.LogEntry;
+import com.ctrip.platform.dal.dao.markdown.MarkDownInfo;
+import com.ctrip.platform.dal.dao.markdown.MarkupInfo;
+import com.dianping.cat.Cat;
+import com.dianping.cat.CatConstants;
+import com.dianping.cat.message.Message;
+import com.dianping.cat.message.Transaction;
+
+public class DalCatLogger {
+	public static void start(CtripLogEntry entry) {
+		try {
+			String sqlType = entry.getDao() + "." + entry.getMethod();
+			Transaction catTransaction = Cat.newTransaction(CatConstants.TYPE_SQL, sqlType);
+			entry.setCatTransaction(catTransaction);
+			catTransaction.addData(entry.getSqls() == null ? "" : StringUtils.join(entry.getSqls(), ";"));
+			catTransaction.addData("\n");
+			if(entry.getPramemters() != null){
+				catTransaction.addData(entry.getEncryptParameters());
+			}
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void catTransactionSuccess(CtripLogEntry entry){
+		try {
+			String method = entry.getEvent() == null ? "dal_test" : CatInfo.getTypeSQLInfo(entry.getEvent());
+			Cat.logEvent("DAL.version", "java-" + entry.getClientVersion());
+			Cat.logEvent(CatConstants.TYPE_SQL_METHOD, method, Message.SUCCESS, "");
+			Cat.logEvent(CatConstants.TYPE_SQL_DATABASE, entry.getDbUrl());
+			entry.getCatTransaction().setStatus(Transaction.SUCCESS);
+			entry.getCatTransaction().complete();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void catTransactionFailed(CtripLogEntry entry, Throwable e){
+		try {
+			entry.getCatTransaction().setStatus(e);
+			Cat.logError(e);
+			entry.getCatTransaction().complete();
+		} catch (Throwable e1) {
+			e1.printStackTrace();
+		}
+	}
+	
+}

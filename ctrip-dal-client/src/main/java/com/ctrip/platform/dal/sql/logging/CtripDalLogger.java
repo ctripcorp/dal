c7590cc647250926d3c19cacc252c4389d7dab94
xgz夏光智 <gzxia@ctrip.com>
2015-04-09 16:25:06 +0800

    support asyn logging

--- ctrip-dal-client/src/main/java/com/ctrip/platform/dal/sql/logging/CtripDalLogger.java
+++ ctrip-dal-client/src/main/java/com/ctrip/platform/dal/sql/logging/CtripDalLogger.java
@@ -1,6 +1,8 @@
 package com.ctrip.platform.dal.sql.logging;
 
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.ctrip.framework.clogging.domain.thrift.LogLevel;
 import com.ctrip.platform.dal.dao.client.DalLogger;
@@ -12,6 +14,8 @@ public class CtripDalLogger implements DalLogger {
 	private static final String SAMPLING = "sampling";
 	private static final String ENCRYPT = "encrypt";
 	private static final String SIMPLIFIED = "simplified";
+	
+	private static final ExecutorService executor = Executors.newSingleThreadExecutor();
 	
 	@Override
 	public void initLogger(Map<String, String> settings) {
@@ -30,23 +34,43 @@ public class CtripDalLogger implements DalLogger {
 	}
 
 	@Override
-	public void info(String msg) {
-		DalCLogger.log(LogLevel.INFO, msg);
+	public void info(final String msg) {
+		executor.submit(new Runnable() {
+			@Override
+			public void run() {
+				DalCLogger.log(LogLevel.INFO, msg);
+			}
+		});
+	}
+	
+	@Override
+	public void warn(final String msg) {
+		executor.submit(new Runnable() {
+			@Override
+			public void run() {
+				DalCLogger.log(LogLevel.WARN, msg);
+			}
+		});
 	}
 
 	@Override
-	public void warn(String msg) {
-		DalCLogger.log(LogLevel.WARN, msg);
+	public void error(final String msg, final Throwable e) {
+		executor.submit(new Runnable() {
+			@Override
+			public void run() {
+				DalCLogger.error(msg, e);	
+			}
+		});
 	}
 
 	@Override
-	public void error(String msg, Throwable e) {
-		DalCLogger.error(msg, e);	
-	}
-
-	@Override
-	public void getConnectionFailed(String dbName, Throwable e) {
-		DalCLogger.getConnectionFailed(dbName, e);
+	public void getConnectionFailed(final String dbName, final Throwable e) {
+		executor.submit(new Runnable() {
+			@Override
+			public void run() {
+				DalCLogger.getConnectionFailed(dbName, e);
+			}
+		});
 	}
 
 	@Override
@@ -55,46 +79,69 @@ public class CtripDalLogger implements DalLogger {
 	}
 
 	@Override
-	public void start(LogEntry entry) {
-		DalCatLogger.start((CtripLogEntry)entry);
+	public void start(final LogEntry entry) {
+		executor.submit(new Runnable() {
+			@Override
+			public void run() {
+				DalCatLogger.start((CtripLogEntry)entry);
+			}
+		});
 	}
 	
 	@Override
-	public void success(LogEntry entry, int count) {
-		try {
-			DalCLogger.success((CtripLogEntry)entry, count);
-			Metrics.success((CtripLogEntry)entry, entry.getDuration());
-			DalCatLogger.catTransactionSuccess((CtripLogEntry)entry);
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
+	public void success(final LogEntry entry, final int count) {
+		executor.submit(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					DalCLogger.success((CtripLogEntry)entry, count);
+					Metrics.success((CtripLogEntry)entry, entry.getDuration());
+					DalCatLogger.catTransactionSuccess((CtripLogEntry)entry);
+				} catch (Throwable e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 	@Override
-	public void fail(LogEntry entry, Throwable e) {
-		try {
-			DalCLogger.fail((CtripLogEntry)entry, e);
-			Metrics.fail((CtripLogEntry)entry, entry.getDuration());
-			DalCatLogger.catTransactionFailed((CtripLogEntry)entry, e);
-		} catch (Throwable ex) {
-			ex.printStackTrace();
-		}
+	public void fail(final LogEntry entry, final Throwable e) {
+		executor.submit(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					DalCLogger.fail((CtripLogEntry)entry, e);
+					Metrics.fail((CtripLogEntry)entry, entry.getDuration());
+					DalCatLogger.catTransactionFailed((CtripLogEntry)entry, e);
+				} catch (Throwable ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
 	}
 
 	@Override
-	public void markdown(MarkDownInfo markdown) {
-		Metrics.report(markdown);
-		
+	public void markdown(final MarkDownInfo markdown) {
+		executor.submit(new Runnable() {
+			@Override
+			public void run() {
+				Metrics.report(markdown);
+			}
+		});
 	}
 
 	@Override
-	public void markup(MarkupInfo markup) {
-		Metrics.report(markup);
-		
+	public void markup(final MarkupInfo markup) {
+		executor.submit(new Runnable() {
+			@Override
+			public void run() {
+				Metrics.report(markup);
+			}
+		});
 	}
 
 	@Override
 	public void shutdown() {
-		// TODO Auto-generated method stub
+		executor.shutdown();
 	}
 }

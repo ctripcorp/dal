08684e97da462ba0911d02de3084e4012d6a55a2
xgz夏光智 <gzxia@ctrip.com>
2015-04-27 11:18:29 +0800

    add async dao test to dal-tester project.

---
+++ dal-tester/src/test/java/com/ctrip/platform/dal/async/dao/People.java
@@ -0,0 +1,53 @@
+package com.ctrip.platform.dal.async.dao;
+
+import com.ctrip.platform.dal.dao.DalPojo;
+
+public class People implements DalPojo {
+	private Long peopleID;
+	private String name;
+	private Integer cityID;
+	private Integer provinceID;
+	private Integer countryID;
+	public Long getPeopleID() {
+		return peopleID;
+	}
+
+	public void setPeopleID(Long peopleID) {
+		this.peopleID = peopleID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getCityID() {
+		return cityID;
+	}
+
+	public void setCityID(Integer cityID) {
+		this.cityID = cityID;
+	}
+
+	public Integer getProvinceID() {
+		return provinceID;
+	}
+
+	public void setProvinceID(Integer provinceID) {
+		this.provinceID = provinceID;
+	}
+
+	public Integer getCountryID() {
+		return countryID;
+	}
+
+	public void setCountryID(Integer countryID) {
+		this.countryID = countryID;
+	}
+
+
+
+}
---
+++ dal-tester/src/test/java/com/ctrip/platform/dal/async/dao/SingleUpdateSpaAsyncTaskTest.java
@@ -0,0 +1,77 @@
+package com.ctrip.platform.dal.async.dao;
+
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.ctrip.platform.dal.dao.DalClient;
+import com.ctrip.platform.dal.dao.DalClientFactory;
+import com.ctrip.platform.dal.dao.DalHints;
+import com.ctrip.platform.dal.dao.SingleUpdateSpaTask;
+import com.ctrip.platform.dal.dao.StatementParameters;
+
+public class SingleUpdateSpaAsyncTaskTest {
+	private final static String DATABASE_NAME = "SimpleShard";
+	
+	private final static String TABLE_NAME = "People";
+	
+	private static DalClient client;
+	static {
+		try {
+			DalClientFactory.initClientFactory();
+			client = DalClientFactory.getClient(DATABASE_NAME);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		String[] insertSqls = new String[4];
+		insertSqls[0] = "SET IDENTITY_INSERT "+ TABLE_NAME + " ON";
+		insertSqls[1] = "DELETE FROM " + TABLE_NAME;
+		insertSqls[2] = "INSERT INTO " + TABLE_NAME +" ([PeopleID], [Name], [CityID], [ProvinceID], [CountryID])"
+					+ " VALUES(" + 1 + ", " + "'test name' , 1, 1, 1)";
+		insertSqls[3] = "SET IDENTITY_INSERT "+ TABLE_NAME + " OFF";
+		client.batchUpdate(insertSqls, new DalHints().inShard(0));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		client.update("DELETE FROM " + TABLE_NAME, new StatementParameters(), new DalHints().inShard(0));
+	}
+	
+	@Test
+	public void testExecute() {
+		SingleUpdateSpaTask<People> test = new SingleUpdateSpaTask<>();
+		PeopleParser parser = new PeopleParser();
+		test.initialize(parser);
+		
+		People p1 = new People();
+	 	p1.setPeopleID((long)1);
+	 	p1.setName("test");
+	 	p1.setCityID(-1);
+	 	p1.setProvinceID(-1);
+	 	p1.setCountryID(-1);
+
+		try {
+			test.execute(new DalHints().inShard(0).asyncExecution(), parser.getFields(p1));
+		} catch (SQLException e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+}

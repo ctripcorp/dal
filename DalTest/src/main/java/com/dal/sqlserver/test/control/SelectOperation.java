a8d89709d076081401301d4e8cb0438c08bace58
He, Jiehui <he_jiehui@163.com>
2016-03-02 17:16:26 +0800

    fix getAll bug

--- DalTest/src/main/java/com/dal/sqlserver/test/control/SelectOperation.java
+++ DalTest/src/main/java/com/dal/sqlserver/test/control/SelectOperation.java
@@ -1,13 +1,8 @@
 package com.dal.sqlserver.test.control;
 
 import java.sql.SQLException;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import com.ctrip.platform.dal.dao.DalHints;
-import com.dal.sqlserver.test.People;
 import com.dal.sqlserver.test.PeopleDao;
 import com.xross.tools.xunit.Context;
 import com.xross.tools.xunit.Processor;
@@ -37,9 +32,6 @@ public class SelectOperation implements Processor {
 				break;
 			case "deleteAll":
 				value = dao.getAll(hints);
-				int pageSize = context.getInt("pageSize"); 
-				int pageNo = context.getInt("pageNo");
-				value  = dao.queryByPage(pageSize, pageNo, hints);
 				break;
 			default:
 				break;

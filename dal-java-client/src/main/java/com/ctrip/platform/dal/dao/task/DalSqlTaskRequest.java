3d9d30aa016e62be8df56e7599f58ff4b21293b2
He, Jiehui <he_jiehui@163.com>
2016-04-29 19:36:45 +0800

    fix

--- dal-java-client/src/main/java/com/ctrip/platform/dal/dao/task/DalSqlTaskRequest.java
+++ dal-java-client/src/main/java/com/ctrip/platform/dal/dao/task/DalSqlTaskRequest.java
@@ -105,14 +105,15 @@ public class DalSqlTaskRequest<T> implements DalRequest<T>{
 	}
 	
 	private Callable<T> create(StatementParameters parameters, DalHints hints) throws SQLException {
-		if(builder == null || !(builder instanceof TableSqlBuilder))
+		if(builder == null)
 			return new SqlTaskCallable<>(DalClientFactory.getClient(logicDbName), sql, parameters, hints, task);
 
-		if(!isTableShardingEnabled(logicDbName, ((TableSqlBuilder)builder).getTableName()))
-			return new SqlTaskCallable<>(DalClientFactory.getClient(logicDbName), builder.build(), parameters, hints, task);
-			
-		String tableShardStr = buildShardStr(logicDbName, locateTableShardId(logicDbName, hints, parameters, null));
-		return new SqlTaskCallable<>(DalClientFactory.getClient(logicDbName), ((TableSqlBuilder)builder).build(tableShardStr), parameters, hints, task);
+		if(builder instanceof TableSqlBuilder && isTableShardingEnabled(logicDbName, ((TableSqlBuilder)builder).getTableName())){
+			String tableShardStr = buildShardStr(logicDbName, locateTableShardId(logicDbName, hints, parameters, null));
+			return new SqlTaskCallable<>(DalClientFactory.getClient(logicDbName), ((TableSqlBuilder)builder).build(tableShardStr), parameters, hints, task);
+		}
+
+		return new SqlTaskCallable<>(DalClientFactory.getClient(logicDbName), builder.build(), parameters, hints, task);
 	}
 
 	@Override

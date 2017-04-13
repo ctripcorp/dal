3e101082dd5e5c149a980cc1f138de786304ec2f
yn.wang <yn.wang@ctrip.com>
2017-04-13 16:18:32 +0800

    Config center introduced.

---
+++ dal-client/src/main/java/com/ctrip/platform/dal/dao/configure/DatabasePoolConfigConstants.java
@@ -0,0 +1,24 @@
+package com.ctrip.platform.dal.dao.configure;
+
+public class DatabasePoolConfigConstants {
+    public static final String TESTWHILEIDLE = "testWhileIdle";
+    public static final String TESTONBORROW = "testOnBorrow";
+    public static final String TESTONRETURN = "testOnReturn";
+    public static final String VALIDATIONQUERY = "validationQuery";
+    public static final String VALIDATIONINTERVAL = "validationInterval";
+    public static final String TIMEBETWEENEVICTIONRUNSMILLIS = "timeBetweenEvictionRunsMillis";
+    public static final String MAX_AGE = "maxAge";
+    public static final String MAXACTIVE = "maxActive";
+    public static final String MINIDLE = "minIdle";
+    public static final String MAXWAIT = "maxWait";
+    public static final String INITIALSIZE = "initialSize";
+    public static final String REMOVEABANDONEDTIMEOUT = "removeAbandonedTimeout";
+    public static final String REMOVEABANDONED = "removeAbandoned";
+    public static final String LOGABANDONED = "logAbandoned";
+    public static final String MINEVICTABLEIDLETIMEMILLIS = "minEvictableIdleTimeMillis";
+    public static final String CONNECTIONPROPERTIES = "connectionProperties";
+    public static final String INIT_SQL = "initSql";
+    public static final String INIT_SQL2 = "initSQL";
+    public static final String OPTION = "option";
+
+}

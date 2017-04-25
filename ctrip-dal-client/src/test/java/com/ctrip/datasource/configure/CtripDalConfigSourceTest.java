65809374c9aa7a61e27dcc16289135f998c7b1d6
yn.wang <yn.wang@ctrip.com>
2017-04-25 16:04:39 +0800

    Added local-mode hack for Dal.config.

--- ctrip-dal-client/src/test/java/com/ctrip/datasource/configure/CtripDalConfigSourceTest.java
+++ ctrip-dal-client/src/test/java/com/ctrip/datasource/configure/CtripDalConfigSourceTest.java
@@ -1,18 +1,24 @@
 package com.ctrip.datasource.configure;
 
+import com.ctrip.platform.dal.dao.configure.DalConfigConstants;
+import com.ctrip.platform.dal.dao.configure.DalConfigureFactory;
 import com.ctrip.platform.dal.dao.configure.DatabaseSet;
 import org.junit.Test;
 
 import java.util.Map;
 
 public class CtripDalConfigSourceTest {
-    @Test
-    public void testGetDatabaseSets() {
-        CtripDalConfigSource configSource = new CtripDalConfigSource();
-        try {
-            Map<String, DatabaseSet> map = configSource.getDatabaseSets(null);
-        } catch (Throwable e) {
-
-        }
+  @Test
+  public void testGetDatabaseSets() throws Exception {
+    CtripDalConfigSource configSource = new CtripDalConfigSource();
+    try {
+      Map<String, DatabaseSet> map = configSource.getDatabaseSets(null);
+    } catch (Throwable e) {
     }
+
+    /*
+     * System.setProperty(DalConfigConstants.USE_LOCAL_DAL_CONFIG, "true"); DalConfigureFactory.load();
+     */
+
+  }
 }

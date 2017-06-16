e5ff99ae9fa34233173b35f70fbfd3a3a70208b4
yn.wang <yn.wang@ctrip.com>
2017-06-16 15:54:44 +0800

    Fixed initSql issue for tomcat jdbc.

--- dal-client/src/main/java/com/ctrip/platform/dal/dao/datasource/DataSourceLocator.java
+++ dal-client/src/main/java/com/ctrip/platform/dal/dao/datasource/DataSourceLocator.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.naming.NamingException;
+import javax.naming.event.ObjectChangeListener;
 import javax.sql.DataSource;
 
 import org.apache.tomcat.jdbc.pool.PoolProperties;
@@ -16,87 +17,145 @@ import com.ctrip.platform.dal.dao.configure.DatabasePoolConfigParser;
 import com.ctrip.platform.dal.dao.configure.DatabasePoolConfig;
 import com.ctrip.platform.dal.dao.configure.DefaultDataSourceConfigureProvider;
 
+import static oracle.net.aso.C07.n;
+import static oracle.net.aso.C07.p;
+
 
 public class DataSourceLocator {
-	private static final Logger logger = LoggerFactory.getLogger(DataSourceLocator.class);
-	
-	private static final ConcurrentHashMap<String,DataSource> cache = new ConcurrentHashMap<String,DataSource>();
-	
-	private DataSourceConfigureProvider provider;
-	
-	public DataSourceLocator(DataSourceConfigureProvider provider) {
-		this.provider = provider;
-	}
-	
-	/**
-	 * This is used for initialize datasource for thirdparty framework
-	 */
-	public DataSourceLocator() {
-		this(new DefaultDataSourceConfigureProvider());
-	}
-	
-	/**
-	 * Get DataSource by real db source name
-	 * @param name
-	 * @return DataSource
-	 * @throws NamingException
-	 */
-	public DataSource getDataSource(String name) throws Exception {
-		DataSource ds = cache.get(name); 
-		
-		if (ds != null) {
-			return ds;
-		}
-		
-		synchronized (this.getClass()) {
-			ds = cache.get(name); 
-			if (ds != null) {
-				return ds;
-			}
-			try {
-				ds = createDataSource(name);
-				cache.put(name, ds);
-			} catch (Throwable e) {
-				String msg = "Creating DataSource "+name+" error:"+e.getMessage();
-				logger.error(msg, e);
-				throw new RuntimeException(msg, e);
-			}
-		}
-		return ds;
-	}
-	
-	private DataSource createDataSource(String name) throws SQLException {
-		DatabasePoolConfig poolConfig = DatabasePoolConfigParser.getInstance().getDatabasePoolConifg(name);
-		DataSourceConfigure config = provider.getDataSourceConfigure(name);
-		
-		if (config == null && poolConfig == null) {
-			throw new SQLException("Can not find any connection configure for " + name);
-		}
-		
-		if (poolConfig == null) {
-			// Create default connection pool configure
-			poolConfig = new DatabasePoolConfig();
-		}
-		
-		PoolProperties p = poolConfig.getPoolProperties();
-		
-		/**
-		 * It is assumed that user name/password/url/driver class name are provided in pool config
-		 * If not, it should be provided by the config provider
-		 */
-		if (config != null) {
-			p.setUrl(config.getConnectionUrl());
-	        p.setUsername(config.getUserName());
-	        p.setPassword(config.getPassword());
-	        p.setDriverClassName(config.getDriverClass());
-		}
-		
+    private static final Logger logger = LoggerFactory.getLogger(DataSourceLocator.class);
+
+    private static final ConcurrentHashMap<String, DataSource> cache = new ConcurrentHashMap<String, DataSource>();
+
+    private static final Object LOCK = new Object();
+    private static final Object LOCK2 = new Object();
+
+    private static ConcurrentHashMap<String, ConcurrentHashMap<String, PoolProperties>> poolPropertiesMap =
+            new ConcurrentHashMap<>();
+
+    private DataSourceConfigureProvider provider;
+
+    public DataSourceLocator(DataSourceConfigureProvider provider) {
+        this.provider = provider;
+    }
+
+    /**
+     * This is used for initialize datasource for thirdparty framework
+     */
+    public DataSourceLocator() {
+        this(new DefaultDataSourceConfigureProvider());
+    }
+
+    /**
+     * Get DataSource by real db source name
+     * 
+     * @param name
+     * @return DataSource
+     * @throws NamingException
+     */
+    public DataSource getDataSource(String name) throws Exception {
+        DataSource ds = cache.get(name);
+
+        if (ds != null) {
+            return ds;
+        }
+
+        synchronized (this.getClass()) {
+            ds = cache.get(name);
+            if (ds != null) {
+                return ds;
+            }
+            try {
+                ds = createDataSource(name);
+                cache.put(name, ds);
+            } catch (Throwable e) {
+                String msg = "Creating DataSource " + name + " error:" + e.getMessage();
+                logger.error(msg, e);
+                throw new RuntimeException(msg, e);
+            }
+        }
+        return ds;
+    }
+
+    private DataSource createDataSource(String name) throws SQLException {
+        DatabasePoolConfig poolConfig = DatabasePoolConfigParser.getInstance().getDatabasePoolConifg(name);
+        DataSourceConfigure config = provider.getDataSourceConfigure(name);
+
+        if (config == null && poolConfig == null) {
+            throw new SQLException("Can not find any connection configure for " + name);
+        }
+
+        if (poolConfig == null) {
+            // Create default connection pool configure
+            poolConfig = new DatabasePoolConfig();
+        }
+
+        PoolProperties p = poolConfig.getPoolProperties();
+
+        /**
+         * It is assumed that user name/password/url/driver class name are provided in pool config If not, it should be
+         * provided by the config provider
+         */
+        if (config != null) {
+            p.setUrl(config.getConnectionUrl());
+            p.setUsername(config.getUserName());
+            p.setPassword(config.getPassword());
+            p.setDriverClassName(config.getDriverClass());
+        }
+
+        setPoolProperties(p);
+
         org.apache.tomcat.jdbc.pool.DataSource ds = new org.apache.tomcat.jdbc.pool.DataSource(p);
-		
+
         ds.createPool();
-        
+
         logger.info("Datasource[name=" + name + ", Driver=" + p.getDriverClassName() + "] created.");
-		
-		return ds;
-	}
+
+        return ds;
+    }
+
+    private void setPoolProperties(PoolProperties poolProperties) {
+        if (poolProperties == null)
+            return;
+
+        String url = poolProperties.getUrl();
+        if (url == null || url.length() == 0)
+            return;
+
+        String userName = poolProperties.getUsername();
+        if (userName == null || userName.length() == 0)
+            return;
+
+        ConcurrentHashMap<String, PoolProperties> map = poolPropertiesMap.get(url);
+
+        if (map == null) {
+            synchronized (LOCK) {
+                map = poolPropertiesMap.get(url);
+                if (map == null) {
+                    map = new ConcurrentHashMap<>();
+                    poolPropertiesMap.put(url, map);
+                }
+            }
+        }
+
+        if (!map.containsKey(userName)) {
+            synchronized (LOCK2) {
+                if (!map.containsKey(userName)) {
+                    map.put(userName, poolProperties);
+                }
+            }
+        }
+    }
+
+    public static PoolProperties getPoolProperties(String url, String userName) {
+        if (url == null || url.length() == 0)
+            return null;
+        if (userName == null || userName.length() == 0)
+            return null;
+
+        ConcurrentHashMap<String, PoolProperties> map = poolPropertiesMap.get(url);
+        if (map == null)
+            return null;
+        return map.get(userName);
+    }
+
 }
--- dal-client/src/main/java/com/ctrip/platform/dal/dao/datasource/DataSourceValidator.java
+++ dal-client/src/main/java/com/ctrip/platform/dal/dao/datasource/DataSourceValidator.java
@@ -1,10 +1,13 @@
 package com.ctrip.platform.dal.dao.datasource;
 
+import org.apache.tomcat.jdbc.pool.PoolProperties;
+import org.apache.tomcat.jdbc.pool.PooledConnection;
 import org.apache.tomcat.jdbc.pool.Validator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
+import java.sql.Statement;
 
 public class DataSourceValidator implements Validator {
     private static final Logger LOGGER = LoggerFactory.getLogger(DataSourceValidator.class);
@@ -14,6 +17,7 @@ public class DataSourceValidator implements Validator {
     public boolean validate(Connection connection, int validateAction) {
         boolean isValid = false;
         try {
+            checkInitSQL(connection, validateAction);
             isValid = connection.isValid(DEFAULT_VALIDATE_TIMEOUT_IN_SECONDS);
         } catch (Throwable ex) {
             LOGGER.warn("Datasource validation error", ex);
@@ -21,4 +25,55 @@ public class DataSourceValidator implements Validator {
 
         return isValid;
     }
+
+    private void checkInitSQL(Connection connection, int validateAction) {
+        if (validateAction != PooledConnection.VALIDATE_INIT) {
+            return;
+        }
+
+        String url = null;
+        String userName = null;
+        try {
+            url = connection.getMetaData().getURL();
+            userName = connection.getMetaData().getUserName();
+        } catch (Throwable e) {
+            LOGGER.warn("Datasource initSQL error", e);
+            return;
+        }
+
+        PoolProperties poolProperties = DataSourceLocator.getPoolProperties(url, userName);
+        if (poolProperties == null)
+            return;
+
+        String query = poolProperties.getInitSQL();
+
+        if (query == null) {
+            return;
+        }
+
+        Statement stmt = null;
+        try {
+            stmt = connection.createStatement();
+
+            int validationQueryTimeout = poolProperties.getValidationQueryTimeout();
+            if (validationQueryTimeout > 0) {
+                stmt.setQueryTimeout(validationQueryTimeout);
+            }
+
+            stmt.execute(query);
+            stmt.close();
+        } catch (Throwable ex) {
+            if (poolProperties.getLogValidationErrors()) {
+                LOGGER.warn("SQL Validation error", ex);
+            } else if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Unable to validate object:", ex);
+            }
+            if (stmt != null)
+                try {
+                    stmt.close();
+                } catch (Throwable ignore2) {
+                    /* NOOP */}
+        }
+    }
+
 }

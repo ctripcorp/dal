3433bd35fb72898e68b322d4479887bb5c3dc383
yn.wang <yn.wang@ctrip.com>
2017-12-26 20:55:54 +0800

    Added debug log.

--- dal-client/src/main/java/com/ctrip/platform/dal/dao/datasource/DataSourceLocator.java
+++ dal-client/src/main/java/com/ctrip/platform/dal/dao/datasource/DataSourceLocator.java
@@ -45,7 +45,7 @@ public class DataSourceLocator {
 
         if (ds != null) {
             String url = ds.getConnection().getMetaData().getURL();
-            logger.debug("**********" + name + ":" + url);
+            logger.info("**********" + name + ":" + url);
             return ds;
         }
 
@@ -65,7 +65,7 @@ public class DataSourceLocator {
         }
 
         String url = ds.getConnection().getMetaData().getURL();
-        logger.debug("**********" + name + ":" + url);
+        logger.info("**********" + name + ":" + url);
         return ds;
     }
 

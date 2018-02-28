99894c59bc986d4db5ecd99ae642d156fa6da171
yn.wang <yn.wang@ctrip.com>
2018-02-28 18:44:40 +0800

    Renamed switch name to dal_ip_status.

--- ctrip-datasource/src/main/java/com/ctrip/datasource/configure/qconfig/IPDomainStatusProviderImpl.java
+++ ctrip-datasource/src/main/java/com/ctrip/datasource/configure/qconfig/IPDomainStatusProviderImpl.java
@@ -7,7 +7,7 @@ import qunar.tc.qconfig.client.staticswitch.StaticSwitch;
 import qunar.tc.qconfig.client.staticswitch.StatusChangeListener;
 
 public class IPDomainStatusProviderImpl implements IPDomainStatusProvider {
-    private static final String SWITCH_KEYNAME = "dal";
+    private static final String SWITCH_KEYNAME = "dal_ip_status";
     private StaticSwitch statusSwitch = StaticSwitch.getSwitch(SWITCH_KEYNAME);
 
     @Override

1b88a44bf58a5be2ec01b2242a1c13c6f2aa4481
yn.wang <yn.wang@ctrip.com>
2018-03-01 15:04:41 +0800

    Added missing log.

--- dal-client/src/main/java/com/ctrip/platform/dal/dao/datasource/ConnectionListener.java
+++ dal-client/src/main/java/com/ctrip/platform/dal/dao/datasource/ConnectionListener.java
@@ -5,7 +5,6 @@ import com.ctrip.platform.dal.dao.helper.Ordered;
 import java.sql.Connection;
 
 public interface ConnectionListener extends Ordered {
-
     void onCreateConnection(String poolDesc, Connection connection);
 
     void onReleaseConnection(String poolDesc, Connection connection);
--- dal-client/src/main/java/com/ctrip/platform/dal/dao/datasource/DefaultConnectionListener.java
+++ dal-client/src/main/java/com/ctrip/platform/dal/dao/datasource/DefaultConnectionListener.java
@@ -1,37 +1,10 @@
 package com.ctrip.platform.dal.dao.datasource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.sql.Connection;
-
 public class DefaultConnectionListener extends AbstractConnectionListener implements ConnectionListener {
-
-    private static Logger logger = LoggerFactory.getLogger(DefaultConnectionListener.class);
-
-    @Override
-    public void doOnCreateConnection(String poolDesc, Connection connection) {
-        logInfo("[onCreateConnection]{}, {}", poolDesc, connection);
-    }
-
-    @Override
-    public void doOnReleaseConnection(String poolDesc, Connection connection) {
-        logInfo("[onReleaseConnection]{}, {}", poolDesc, connection);
-    }
-
-    @Override
-    protected void doOnAbandonConnection(String poolDesc, Connection connection) {
-        logInfo("[onAbandonConnection]{}, {}", poolDesc, connection);
-    }
 
     @Override
     public int getOrder() {
         return LOWEST_PRECEDENCE;
-    }
-
-    private void logInfo(String format, String poolDesc, Connection connection) {
-        String connDesc = connectionDesc(connection);
-        logger.info(format, poolDesc, connDesc);
     }
 
 }

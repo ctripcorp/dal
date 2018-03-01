c000ef6f89021dd6b7bf4025d943c3e756337940
yn.wang <yn.wang@ctrip.com>
2018-03-01 11:33:32 +0800

    Added missing log.

--- dal-client/src/main/java/com/ctrip/platform/dal/dao/datasource/DefaultDalConnectionLocator.java
+++ dal-client/src/main/java/com/ctrip/platform/dal/dao/datasource/DefaultDalConnectionLocator.java
@@ -8,15 +8,11 @@ import com.ctrip.platform.dal.dao.client.DalConnectionLocator;
 import com.ctrip.platform.dal.dao.configure.DataSourceConfigureProvider;
 import com.ctrip.platform.dal.dao.configure.DefaultDataSourceConfigureProvider;
 import com.ctrip.platform.dal.dao.helper.ConnectionStringKeyHelper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 
 public class DefaultDalConnectionLocator implements DalConnectionLocator {
-    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultDalConnectionLocator.class);
     private static final String DATASOURCE_CONFIG_PROVIDER = "dataSourceConfigureProvider";
-    private static final String DATASOURCE_GET_DATASOURCE = "DataSource::getDataSource";
 
     private DataSourceConfigureProvider provider;
     private DataSourceLocator locator;
@@ -41,9 +37,7 @@ public class DefaultDalConnectionLocator implements DalConnectionLocator {
     @Override
     public Connection getConnection(String name) throws Exception {
         String keyName = ConnectionStringKeyHelper.getKeyName(name);
-        LOGGER.info(String.format("%s:%s start.", DATASOURCE_GET_DATASOURCE, name));
         DataSource dataSource = locator.getDataSource(keyName);
-        LOGGER.info(String.format("%s:%s end.", DATASOURCE_GET_DATASOURCE, name));
         return dataSource.getConnection();
     }
 

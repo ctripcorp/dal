eebe00b9d63a2e3e610cf203827ec964c17d858d
lilj <lilj@Ctrip.com>
2018-04-01 19:27:47 +0800

    dynamic-datasource-test

---
+++ dal-dynamic-datasource-test/MyBatisWithDalOnMysql/src/main/java/DRTestMybatisMySQL/DRTestMapper.java
@@ -0,0 +1,14 @@
+package DRTestMybatisMySQL;
+
+/**
+ * Created by lilj on 2017/10/26.
+ */
+public interface DRTestMapper {
+    String getHostNameMySQL();
+    String getDatabaseMySQL();
+    DRTestMybatisPojo getDRTestMybatisPojo(int iD);
+    void addDRTestMybatisPojo(DRTestMybatisPojo drTestMybatisPojo);
+    int getCount();
+    void truncateTableMySQL();
+    void updateDRTestMybatisPojo(DRTestMybatisPojo drTestMybatisPojo);
+}

bf927cad43f0d2759a8c341b341cf588fe815f4c
lilj <lilj@Ctrip.com>
2018-07-04 14:48:34 +0800

    modify error method in CtripLoggerImpl

--- ctrip-datasource/src/main/java/com/ctrip/datasource/log/CtripLoggerImpl.java
+++ ctrip-datasource/src/main/java/com/ctrip/datasource/log/CtripLoggerImpl.java
@@ -1,5 +1,7 @@
 package com.ctrip.datasource.log;
 
+import com.ctrip.framework.clogging.agent.log.ILog;
+import com.ctrip.framework.clogging.agent.log.LogManager;
 import com.ctrip.platform.dal.dao.log.AbstractLogger;
 import com.ctrip.platform.dal.dao.log.Callback;
 import com.dianping.cat.Cat;
@@ -7,6 +9,9 @@ import com.dianping.cat.message.Message;
 import com.dianping.cat.message.Transaction;
 
 public class CtripLoggerImpl extends AbstractLogger {
+    public static final String TITLE = "Dal Fx";
+    private static final ILog logger = LogManager.getLogger(CtripLoggerImpl.class);
+
     @Override
     public void logEvent(String type, String name, String message) {
         Cat.logEvent(type, name, Message.SUCCESS, message);
@@ -30,5 +35,39 @@ public class CtripLoggerImpl extends AbstractLogger {
             t.complete();
         }
 
+    }
+
+
+    @Override
+    public void warn(final String msg) {
+        try {
+            logger.warn(msg);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void error(final String msg, final Throwable e) {
+            try {
+                Cat.logError(e);
+            } catch (Throwable ex) {
+                ex.printStackTrace();
+            }
+
+            try {
+                logger.error(TITLE, e);
+            } catch (Throwable ex) {
+                ex.printStackTrace();
+            }
+    }
+
+    @Override
+    public void info(final String msg) {
+        try {
+            logger.info(TITLE, msg);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
     }
 }

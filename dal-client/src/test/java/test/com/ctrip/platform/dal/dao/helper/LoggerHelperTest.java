315e4285a5fa15ecfa84a5092d73c7658bfe3905
lilj <lilj@Ctrip.com>
2018-07-09 15:52:20 +0800

    delete db message

--- dal-client/src/test/java/test/com/ctrip/platform/dal/dao/helper/LoggerHelperTest.java
+++ dal-client/src/test/java/test/com/ctrip/platform/dal/dao/helper/LoggerHelperTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertEquals;
 public class LoggerHelperTest {
     @Test
     public void testToJSon() throws Exception {
-        String finalString = "{\"HasSql\":\"1\",\"Hash\":\"\",\"SqlTpl\":\"/*100008424-DRTestDao.insert[MSG_ID:100008424-0a201579-423702-100004]*/INSERT INTO `testTable` (`Name`) VALUES(?)\",\"Param\":\"aXuR5QP4NhJhsYi3H+rKEdHG/06mtU6m\",\"IsSuccess\":\"1\",\"ErrorMsg\":\"\",\"CostDetail\":\"{'Decode'='16', 'Connect'='517', 'Prepare'='1', 'Execute'='11', 'ClearUp'='2'}\"}";
+        String finalString = "{\"HasSql\":\"1\",\"Hash\":\"\",\"SqlTpl\":\"/*100008424-DRTestDao.insert[MSG_ID:123456]*/INSERT INTO `testTable` (`Name`) VALUES(?)\",\"Param\":\"aXuR5QP4NhJhsYi3H+rKEdHG/06mtU6m\",\"IsSuccess\":\"1\",\"ErrorMsg\":\"\",\"CostDetail\":\"{'Decode'='16', 'Connect'='517', 'Prepare'='1', 'Execute'='11', 'ClearUp'='2'}\"}";
 
         Map<String, String> costDetailMap = new LinkedHashMap();
         costDetailMap.put("'Decode'", "'" + Long.toString(16l) + "'");
@@ -27,7 +27,7 @@ public class LoggerHelperTest {
         Map<String, String> logMap = new LinkedHashMap<>();
         logMap.put("HasSql", "1");
         logMap.put("Hash", "");
-        logMap.put("SqlTpl", "/*100008424-DRTestDao.insert[MSG_ID:100008424-0a201579-423702-100004]*/INSERT INTO `testTable` (`Name`) VALUES(?)");
+        logMap.put("SqlTpl", "/*100008424-DRTestDao.insert[MSG_ID:123456]*/INSERT INTO `testTable` (`Name`) VALUES(?)");
         logMap.put("Param", "aXuR5QP4NhJhsYi3H+rKEdHG/06mtU6m");
         logMap.put("IsSuccess", "1");
         logMap.put("ErrorMsg", "");

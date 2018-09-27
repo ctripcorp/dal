e9cebcdc70ec2a75cf2e73dbcb76d70365462961
c7ch23en <cchen723@163.com>
2018-09-27 18:11:39 +0800

    change idgen version to 1.1.0-SNAPSHOT

---
+++ ctrip-idgen/ctrip-idgen-server/src/test/java/com/ctrip/framework/idgen/server/LocalServiceInitializer.java
@@ -0,0 +1,41 @@
+package com.ctrip.framework.idgen.server;
+
+import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
+import com.ctrip.framework.cdubbo.spring.annotation.EnableCDubbo;
+import com.ctrip.framework.idgen.server.service.IdFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@DubboComponentScan
+@EnableCDubbo
+public class LocalServiceInitializer extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        setEnvironment();
+        IdFactory factory = IdFactory.getInstance();
+        factory.initialize();
+        Long id1 = factory.getOrCreateIdWorker("testName1").generateIdPool(10, 100).get(0).getEnd().longValue();
+        Long id2 = factory.getOrCreateIdWorker("daltestdb1.daltesttb1").generateIdPool(10, 100).get(0).getEnd().longValue();
+        SpringApplication.run(LocalServiceInitializer.class);
+    }
+
+    private static void setEnvironment() {
+        System.setProperty("java.awt.headless", "false");
+        overrideArtemisUrl("10.2.35.218");
+    }
+
+    private static void overrideArtemisUrl(String ip) {
+        String url = String.format("http://%s:8080/artemis-service/", ip);
+        System.setProperty("artemis.client.cdubbo.service.service.domain.url", url);
+        System.setProperty("artemis.client.cdubbo.client.service.domain.url", url);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LocalServiceInitializer.class);
+    }
+
+}

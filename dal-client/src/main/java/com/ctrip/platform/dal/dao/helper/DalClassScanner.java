e7f8c803f920b6c5d921eaa1714cc10898d63d53
c7ch23en <cchen723@163.com>
2018-10-26 20:42:27 +0800

    parallelly scan classes

--- dal-client/src/main/java/com/ctrip/platform/dal/dao/helper/DalClassScanner.java
+++ dal-client/src/main/java/com/ctrip/platform/dal/dao/helper/DalClassScanner.java
@@ -12,6 +12,13 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
@@ -26,12 +33,15 @@ public class DalClassScanner implements ClassScanner {
     private static final String PACKAGE_SEPARATOR = ".";
     private static final String PATH_SEPARATOR = "/";
     private static final String EMPTY_PACKAGE = "";
+    private static final long URL_SCAN_TIMEOUT_SECONDS = 1;
 
     private FileFilter classFileOrDirectoryFilter = new ClassFileOrDirectoryFilter();
     private ClassScanFilter classScanFilter;
     private ClassLoader classLoader;
-    private int fileCount;
-    private int jarCount;
+    private AtomicInteger fileCount = new AtomicInteger(0);
+    private AtomicInteger jarCount = new AtomicInteger(0);
+
+    private ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public DalClassScanner() {
         this(null, null);
@@ -50,10 +60,12 @@ public class DalClassScanner implements ClassScanner {
         this.classLoader = classLoader != null ? classLoader : Thread.currentThread().getContextClassLoader();
     }
 
+    @Override
     public List<Class<?>> getClasses(String packageName, boolean recursive) {
-        fileCount = 0;
-        jarCount = 0;
-        List<Class<?>> classList = new ArrayList<>();
+        long startTime = System.currentTimeMillis();
+        fileCount.set(0);
+        jarCount.set(0);
+        List<Class<?>> classList = new Vector<>();
         if (null == packageName || packageName.trim().isEmpty()) {
             throw new IllegalArgumentException("packageName should not be null or empty");
         }
@@ -61,84 +73,107 @@ public class DalClassScanner implements ClassScanner {
         String packagePath = packageName.replace(PACKAGE_SEPARATOR, PATH_SEPARATOR);
         try {
             Enumeration<URL> urls = classLoader.getResources(packagePath);
+            List<URL> urlList = new ArrayList<>();
             while (urls.hasMoreElements()) {
                 URL url = urls.nextElement();
                 if (url != null) {
-                    String protocol = url.getProtocol();
-                    String path = url.getPath();
-                    if (URL_PROTOCOL_FILE.equals(protocol)) {
-                        findClasses(classList, packageName, path, recursive);
-                    } else if (URL_PROTOCOL_JAR.equals(protocol)) {
-                        findClasses(classList, packageName, url, recursive);
-                    }
+                    urlList.add(url);
                 }
             }
+            findClasses(classList, packageName, urlList, recursive);
         } catch (IOException e) {
             throw new DalRuntimeException(e);
         }
+        long endTime = System.currentTimeMillis();
 
         LOGGER.info("=========================================================================");
+        LOGGER.info(String.format("Time cost: %d ms", endTime - startTime));
         LOGGER.info(String.format("%d classes found", classList.size()));
-        LOGGER.info(String.format("%d files scanned", fileCount));
-        LOGGER.info(String.format("%d jars scanned", jarCount));
+        LOGGER.info(String.format("%d files scanned", fileCount.get()));
+        LOGGER.info(String.format("%d jars scanned", jarCount.get()));
         LOGGER.info("=========================================================================");
         return classList;
     }
 
-    private void findClasses(List<Class<?>> classList, String packageName, String path, boolean recursive) {
-        try {
-            File[] files = new File(path).listFiles(classFileOrDirectoryFilter);
-            if (files != null) {
-                for (File file : files) {
-                    try {
-                        String fileName = file.getName();
-                        fileCount++;
-                        if (file.isFile()) {
-                            LOGGER.info("Scanning file: " + file.getPath());
-                            String className = getClassName(packageName, fileName);
-                            tryAddClass(classList, className);
-                        } else if (recursive) {
-                            String subPackageName = packageName.isEmpty() ?
-                                    fileName : packageName + PACKAGE_SEPARATOR + fileName;
-                            String subPath = path.endsWith(PATH_SEPARATOR) ?
-                                    path + fileName : path + PATH_SEPARATOR + fileName;
-                            findClasses(classList, subPackageName, subPath, recursive);
+    private void findClasses(final List<Class<?>> classList, final String packageName,
+                             final List<URL> urls, final boolean recursive) {
+        int urlCount = urls.size();
+        if (urlCount > 0) {
+            final CountDownLatch latch = new CountDownLatch(urlCount);
+            for (final URL url : urls) {
+                executor.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            String protocol = url.getProtocol();
+                            String path = url.getPath();
+                            if (URL_PROTOCOL_FILE.equals(protocol)) {
+                                findClasses(classList, packageName, path, recursive);
+                            } else if (URL_PROTOCOL_JAR.equals(protocol)) {
+                                findClasses(classList, packageName, url, recursive);
+                            }
+                        } catch (Throwable t) {
+                            LOGGER.error(t.getMessage(), t);
                         }
-                    } catch (Throwable t) {
-                        LOGGER.warn(t.getMessage(), t);
+                        latch.countDown();
                     }
-                }
+                });
             }
-        } catch (Throwable t) {
-            LOGGER.warn(t.getMessage(), t);
+            try {
+                latch.await(URL_SCAN_TIMEOUT_SECONDS, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                LOGGER.error(e.getMessage(), e);
+                Thread.currentThread().interrupt();
+            }
         }
     }
 
-    private void findClasses(List<Class<?>> classList, String packageName, URL url, boolean recursive) {
-        try {
-            JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
-            JarFile jarFile = jarURLConnection.getJarFile();
-            Enumeration<JarEntry> jarEntries = jarFile.entries();
-            LOGGER.info("Scanning jar: " + jarFile.getName());
-            jarCount++;
-            while (jarEntries.hasMoreElements()) {
+    private void findClasses(List<Class<?>> classList, String packageName, String path, boolean recursive) {
+        File[] files = new File(path).listFiles(classFileOrDirectoryFilter);
+        if (files != null) {
+            for (File file : files) {
                 try {
-                    JarEntry jarEntry = jarEntries.nextElement();
-                    String jarEntryName = jarEntry.getName();
-                    String jarEntryClassName = getClassName(jarEntryName);
-                    if (jarEntryClassName != null) {
-                        String jarEntryPackageName = getPackageName(jarEntryClassName);
-                        if (jarEntryPackageName.equals(packageName) ||
-                                (recursive && jarEntryPackageName.startsWith(packageName))) {
-                            tryAddClass(classList, jarEntryClassName);
-                        }
+                    String fileName = file.getName();
+                    fileCount.incrementAndGet();
+                    if (file.isFile()) {
+//                        LOGGER.info("Scanning file: " + file.getPath());
+                        String className = getClassName(packageName, fileName);
+                        tryAddClass(classList, className);
+                    } else if (recursive) {
+                        String subPackageName = packageName.isEmpty() ?
+                                fileName : packageName + PACKAGE_SEPARATOR + fileName;
+                        String subPath = path.endsWith(PATH_SEPARATOR) ?
+                                path + fileName : path + PATH_SEPARATOR + fileName;
+                        findClasses(classList, subPackageName, subPath, recursive);
                     }
                 } catch (Throwable t) {
                     LOGGER.warn(t.getMessage(), t);
                 }
             }
-        } catch (Throwable t) {
-            LOGGER.warn(t.getMessage(), t);
+        }
+    }
+
+    private void findClasses(List<Class<?>> classList, String packageName, URL url, boolean recursive) throws Throwable {
+        JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+        JarFile jarFile = jarURLConnection.getJarFile();
+        Enumeration<JarEntry> jarEntries = jarFile.entries();
+        jarCount.incrementAndGet();
+//        LOGGER.info("Scanning jar: " + jarFile.getName());
+        while (jarEntries.hasMoreElements()) {
+            try {
+                JarEntry jarEntry = jarEntries.nextElement();
+                String jarEntryName = jarEntry.getName();
+                String jarEntryClassName = getClassName(jarEntryName);
+                if (jarEntryClassName != null) {
+                    String jarEntryPackageName = getPackageName(jarEntryClassName);
+                    if (jarEntryPackageName.equals(packageName) ||
+                            (recursive && jarEntryPackageName.startsWith(packageName))) {
+                        tryAddClass(classList, jarEntryClassName);
+                    }
+                }
+            } catch (Throwable t) {
+                LOGGER.warn(t.getMessage(), t);
+            }
         }
     }
 
@@ -179,7 +214,7 @@ public class DalClassScanner implements ClassScanner {
         try {
             clazz = Class.forName(className, false, classLoader);
         } catch (Throwable t) {
-            LOGGER.warn(String.format("Class '%s' cannot be loaded", className));
+//            LOGGER.warn(String.format("Class '%s' cannot be loaded", className));
         }
         if (clazz != null) {
             if (classScanFilter != null) {

53c4bb3fa88e832f7deb954286440326142fea2a
lilj <lilj@Ctrip.com>
2018-12-11 16:40:25 +0800

    fix concurrent bug in DefaultLogSamplingStrategy.validate

--- dal-client/src/main/java/com/ctrip/platform/dal/dao/client/DefaultLogSamplingStrategy.java
+++ dal-client/src/main/java/com/ctrip/platform/dal/dao/client/DefaultLogSamplingStrategy.java
@@ -1,18 +1,13 @@
 package com.ctrip.platform.dal.dao.client;
 
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 public class DefaultLogSamplingStrategy implements ILogSamplingStrategy {
-    private AtomicBoolean started = new AtomicBoolean(false);
-    private LogCacheForDatabaseSetAndTables logCacheForDatabaseSetAndTables;
+    private LogCacheForDatabaseSetAndTables logCacheForDatabaseSetAndTables = new LogCacheForDatabaseSetAndTables();
 
     @Override
     public boolean validate(ILogEntry entry) {
         if (LoggerAdapter.samplingRate == 0)
             return false;
-        if (started.compareAndSet(false, true))
-            logCacheForDatabaseSetAndTables = new LogCacheForDatabaseSetAndTables();
         return logCacheForDatabaseSetAndTables.validateDatabaseSetAndTablesCache(entry);
     }
 

393f2b9a975c1ab2af55d8eba025e8fdab1a3ecd
lilj <lilj@Ctrip.com>
2019-05-16 15:23:21 +0800

    AWS test

--- dal-application/src/main/java/com/ctrip/platform/dal/application/Application.java
+++ dal-application/src/main/java/com/ctrip/platform/dal/application/Application.java
@@ -1,6 +1,12 @@
 package com.ctrip.platform.dal.application;
 
-import org.springframework.boot.SpringApplication;
+import com.ctrip.platform.dal.application.dao.DALServiceDao;
+import com.ctrip.platform.dal.application.entity.DALServiceTable;
+import com.ctrip.platform.dal.application.service.DALService;
+import com.ctrip.platform.dal.dao.DalHints;
+import com.ctrip.platform.dal.dao.DalTableDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -11,8 +17,19 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Application extends SpringBootServletInitializer{
+    private static Logger log = LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) throws Exception{
-        SpringApplication.run(Application.class,args);
+//        SpringApplication.run(Application.class,args);
+        DALServiceDao dao=new DALServiceDao();
+        while(true){
+            try {
+                log.info(String.format("current hostname: %s", dao.selectHostname(new DalHints())));
+            }catch (Exception e){
+                log.error("select hostname error",e);
+            }finally {
+                Thread.sleep(3000);
+            }
+        }
     }
 
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application){

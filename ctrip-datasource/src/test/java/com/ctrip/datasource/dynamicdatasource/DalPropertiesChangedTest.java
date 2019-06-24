2ef5f1091110c426eb26efaa140112436b2527c5
lilj <lilj@ctrip.com>
2019-06-24 14:50:30 +0800

    implicit all shards

--- ctrip-datasource/src/test/java/com/ctrip/datasource/dynamicdatasource/DalPropertiesChangedTest.java
+++ ctrip-datasource/src/test/java/com/ctrip/datasource/dynamicdatasource/DalPropertiesChangedTest.java
@@ -1,9 +1,12 @@
 package com.ctrip.datasource.dynamicdatasource;
 
+import com.ctrip.platform.dal.common.enums.ImplicitAllShardsSwitch;
 import com.ctrip.platform.dal.dao.configure.dalproperties.DalPropertiesManager;
 import com.ctrip.datasource.dynamicdatasource.provider.LocalDalPropertiesProvider;
 import com.ctrip.platform.dal.common.enums.TableParseSwitch;
 import com.ctrip.platform.dal.dao.configure.dalproperties.DalPropertiesLocator;
+import com.ctrip.platform.dal.dao.helper.DalElementFactory;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -19,7 +22,16 @@ public class DalPropertiesChangedTest {
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
+        dalPropertiesManager.tearDown();
         dalPropertiesManager.setDalPropertiesProvider(localDalPropertiesProvider);
+        dalPropertiesManager.setup();
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        dalPropertiesManager.tearDown();
+        dalPropertiesManager.setDalPropertiesProvider(DalElementFactory.DEFAULT.getDalPropertiesProvider());
+        dalPropertiesManager.setup();
     }
 
     @Test
@@ -36,18 +48,30 @@ public class DalPropertiesChangedTest {
         Assert.assertEquals(TableParseSwitch.OFF, locator.getTableParseSwitch());
     }
 
-//    @Test
-//    public void testTableParseSwitchChange() throws Exception {
-//        localDalPropertiesProvider.initStatus();
-//        dalPropertiesManager.setup();
-//        for (int i = 0; i < 10; i++) {
-//            TableParseSwitch initStatus = locator.getTableParseSwitch();
-//            localDalPropertiesProvider.triggerTableParseSwitchChanged();
-//            Thread.sleep(1 * 1000);
-//            TableParseSwitch switchStatus = locator.getTableParseSwitch();
-//            Assert.assertNotEquals(initStatus, switchStatus);
-//        }
-//    }
+    @Test
+    public void testTableParseSwitchChange() throws Exception {
+        localDalPropertiesProvider.initStatus();
+        dalPropertiesManager.setup();
+        for (int i = 0; i < 10; i++) {
+            TableParseSwitch initStatus = locator.getTableParseSwitch();
+            localDalPropertiesProvider.triggerTableParseSwitchChanged();
+            Thread.sleep(1 * 1000);
+            TableParseSwitch switchStatus = locator.getTableParseSwitch();
+            Assert.assertNotEquals(initStatus, switchStatus);
+        }
+    }
 
+    @Test
+    public void testImplicitAllShardsSwitchChange() throws Exception {
+        localDalPropertiesProvider.initStatus();
+        dalPropertiesManager.setup();
+        for (int i = 0; i < 10; i++) {
+            ImplicitAllShardsSwitch initStatus = locator.getImplicitAllShardsSwitch();
+            localDalPropertiesProvider.triggerImplicitAllShardsSwitchChanged();
+            Thread.sleep(1 * 1000);
+            ImplicitAllShardsSwitch switchStatus = locator.getImplicitAllShardsSwitch();
+            Assert.assertNotEquals(initStatus, switchStatus);
+        }
+    }
 
 }
--- ctrip-datasource/src/test/java/com/ctrip/datasource/dynamicdatasource/provider/AbstractDalPropertiesProvider.java
+++ ctrip-datasource/src/test/java/com/ctrip/datasource/dynamicdatasource/provider/AbstractDalPropertiesProvider.java
@@ -2,6 +2,7 @@ package com.ctrip.datasource.dynamicdatasource.provider;
 
 import com.ctrip.platform.dal.dao.configure.dalproperties.DalPropertiesChanged;
 import com.ctrip.platform.dal.dao.configure.dalproperties.DalPropertiesProvider;
+import com.ctrip.platform.dal.dao.configure.dalproperties.DefaultDalPropertiesLocator;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,16 +12,20 @@ import java.util.Map;
  */
 public class AbstractDalPropertiesProvider implements DalPropertiesProvider {
     protected Boolean defaultStatus = true;
-    protected static final String SWITCH_KEYNAME = "TableParseSwitch";
+    protected Boolean defaultImplicitAllShardsStatus = false;
+    protected static final String SWITCH_KEYNAME = DefaultDalPropertiesLocator.TABLE_PARSE_SWITCH_KEYNAME;
+    protected static final String IMPLICIT_ALL_SHARDS_SWITCH_KEYNAME = DefaultDalPropertiesLocator.IMPLICIT_ALL_SHARDS_SWITCH;
 
     @Override
     public Map<String, String> getProperties() {
         Map<String, String> map = new HashMap<>();
         map.put(SWITCH_KEYNAME, defaultStatus.toString());
+        map.put(IMPLICIT_ALL_SHARDS_SWITCH_KEYNAME, defaultImplicitAllShardsStatus.toString());
         return map;
     }
 
     @Override
-    public void addPropertiesChangedListener(DalPropertiesChanged callback) {}
+    public void addPropertiesChangedListener(DalPropertiesChanged callback) {
+    }
 
 }

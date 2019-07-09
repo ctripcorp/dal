d51e9563760cadada97bdfab61af2f9b5eedf4ca
lilj <lilj@Ctrip.com>
2019-07-09 14:40:11 +0800

    add fireman

---
+++ dal-application/src/main/java/com/ctrip/platform/dal/application/fireman/DalFiremanDependency.java
@@ -0,0 +1,46 @@
+package com.ctrip.platform.dal.application.fireman;
+
+import com.ctrip.datasource.configure.DalDataSourceFactory;
+import com.ctrip.framework.fireman.spi.FiremanDependency;
+import com.ctrip.platform.dal.dao.configure.ConnectionStringParser;
+import com.ctrip.platform.dal.dao.datasource.ForceSwitchableDataSource;
+import com.ctrip.platform.dal.exceptions.DalRuntimeException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DalFiremanDependency implements FiremanDependency {
+    private static final String KEY_NAME = "DalService2DB_W";
+
+    @Override
+    public List<String> getAppIds() {
+        List<String> appIds = new ArrayList<>();
+        appIds.add("100015050");
+        return appIds;
+    }
+
+    @Override
+    public String getDatabaseDomainName() {
+        ForceSwitchableDataSource dataSource = getDataSource();
+        String domainConnectionString = dataSource.getSingleDataSource().getDataSourceConfigure().getConnectionString().getDomainConnectionString();
+        String domain = new ConnectionStringParser().parse(KEY_NAME, domainConnectionString).getHostName();
+        Integer port = dataSource.getStatus().getPort();
+        return String.format("%s:%d", domain, port);
+    }
+
+    @Override
+    public ForceSwitchableDataSource getDataSource() {
+        ForceSwitchableDataSource dataSource = null;
+        try {
+            dataSource = (ForceSwitchableDataSource) new DalDataSourceFactory().createDataSource(KEY_NAME);
+        } catch (Exception e) {
+            throw new DalRuntimeException("get datasource error");
+        }
+        return dataSource;
+    }
+
+    @Override
+    public int getOrder() {
+        return HIGHEST_PRECEDENCE;
+    }
+}

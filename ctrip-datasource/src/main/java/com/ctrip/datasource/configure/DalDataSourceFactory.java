4d502087e1d13d7ddfe63b7dac37439f39e4f5f9
chen_cheng <chen_cheng@ctrip.com>
2020-06-02 14:35:03 +0800

    cluster datasource api definition

--- ctrip-datasource/src/main/java/com/ctrip/datasource/configure/DalDataSourceFactory.java
+++ ctrip-datasource/src/main/java/com/ctrip/datasource/configure/DalDataSourceFactory.java
@@ -110,6 +110,17 @@ public class DalDataSourceFactory {
         return locator.getDataSource(id);
     }
 
+    /**
+     * Get or create the DataSource for the only master of the specified cluster.
+     * Exception will be thrown when the cluster is sharding.
+     * @param clusterName dal cluster name
+     * @return DataSource
+     * @throws Exception
+     */
+    public DataSource getOrCreateNonShardingDataSource(String clusterName) throws Exception {
+        return null;
+    }
+
     public DataSource createDataSource(String allInOneKey, String svcUrl, String appid, boolean isForceInitialize) throws Exception {
         TitanProvider provider = initTitanProvider(isForceInitialize);
 

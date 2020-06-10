d1aefbdad67cd45f3268fba1d354e94731bf034e
c7ch23en <cchen723@163.com>
2020-06-10 17:08:12 +0800

    version 2.1.2

--- dal-client/src/test/java/com/ctrip/platform/dal/dao/datasource/jdbc/DalDaoTest.java
+++ dal-client/src/test/java/com/ctrip/platform/dal/dao/datasource/jdbc/DalDaoTest.java
@@ -4,6 +4,7 @@ import com.ctrip.platform.dal.dao.*;
 import com.ctrip.platform.dal.dao.datasource.DataSourceLocator;
 import com.ctrip.platform.dal.dao.datasource.RefreshableDataSource;
 import com.ctrip.platform.dal.dao.helper.FixedValueRowMapper;
+import com.ctrip.platform.dal.dao.sqlbuilder.FreeUpdateSqlBuilder;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -27,32 +28,50 @@ public class DalDaoTest {
         //TestTableDao testTableDao = new TestTableDao();
         DalQueryDao client = new DalQueryDao(dbName);
 
-
         client.query("select 1", new StatementParameters(), new DalHints(), new FixedValueRowMapper<>());
         RefreshableDataSource dataSource = getDataSource();
         Assert.assertEquals(0, dataSource.getFirstAppearContinuousErrorTime());
         Assert.assertEquals(0, dataSource.getLastReportContinuousErrorTime());
 
         try {
-            client.query("select *from noTable", new StatementParameters(), new DalHints(), new FixedValueRowMapper<>());
+            client.query("select * from noTable", new StatementParameters(), new DalHints(), new FixedValueRowMapper<>());
         } catch (SQLException e) {
-
         }
         Assert.assertNotEquals(0, dataSource.getFirstAppearContinuousErrorTime());
+        Assert.assertEquals(1, dataSource.getContinuousErrorCount());
         Assert.assertEquals(0, dataSource.getLastReportContinuousErrorTime());
 
         Thread.sleep(60*1000);
         try {
-            client.query("select *from noTable", new StatementParameters(), new DalHints(), new FixedValueRowMapper<>());
+            client.query("select * from noTable", new StatementParameters(), new DalHints(), new FixedValueRowMapper<>());
         } catch (SQLException e) {
-
         }
         Assert.assertNotEquals(0, dataSource.getFirstAppearContinuousErrorTime());
+        Assert.assertEquals(2, dataSource.getContinuousErrorCount());
+        Assert.assertEquals(0, dataSource.getLastReportContinuousErrorTime());
+
+        try {
+            client.query("select * from noTable", new StatementParameters(), new DalHints(), new FixedValueRowMapper<>());
+        } catch (SQLException e) {
+        }
+        Assert.assertNotEquals(0, dataSource.getFirstAppearContinuousErrorTime());
+        Assert.assertEquals(3, dataSource.getContinuousErrorCount());
         Assert.assertNotEquals(0, dataSource.getLastReportContinuousErrorTime());
 
         client.query("select 1", new StatementParameters(), new DalHints(), new FixedValueRowMapper<>());
-        Assert.assertEquals(0, dataSource.getFirstAppearContinuousErrorTime());
-        Assert.assertEquals(0, dataSource.getLastReportContinuousErrorTime());
+        Assert.assertNotEquals(0, dataSource.getFirstAppearContinuousErrorTime());
+        Assert.assertEquals(3, dataSource.getContinuousErrorCount());
+        Assert.assertNotEquals(0, dataSource.getLastReportContinuousErrorTime());
+
+        try {
+            FreeUpdateSqlBuilder builder = new FreeUpdateSqlBuilder();
+            builder.setTemplate("update noTable set id = 1 where id > 1");
+            client.update(builder, new DalHints());
+        } catch (SQLException e) {
+        }
+        Assert.assertNotEquals(0, dataSource.getFirstAppearContinuousErrorTime());
+        Assert.assertEquals(4, dataSource.getContinuousErrorCount());
+        Assert.assertNotEquals(0, dataSource.getLastReportContinuousErrorTime());
     }
 
     private RefreshableDataSource getDataSource() {

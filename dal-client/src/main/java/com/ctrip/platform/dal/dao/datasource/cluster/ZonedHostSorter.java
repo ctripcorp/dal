b5b49f10e2eb49d09df881c085277a966450d634
c7ch23en <chen_cheng@trip.com>
2020-10-15 22:50:03 +0800

    refine MultiHostDataSource

--- dal-client/src/main/java/com/ctrip/platform/dal/dao/datasource/cluster/ZonedHostSorter.java
+++ dal-client/src/main/java/com/ctrip/platform/dal/dao/datasource/cluster/ZonedHostSorter.java
@@ -1,18 +1,41 @@
 package com.ctrip.platform.dal.dao.datasource.cluster;
 
 import com.ctrip.platform.dal.dao.helper.Sorter;
+import com.ctrip.platform.dal.dao.helper.StringValueComparator;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author c7ch23en
  */
 public class ZonedHostSorter implements Sorter<HostSpec> {
 
+    private final List<String> zoneOrder;
+    private final Comparator<HostSpec> comparator = new StringValueComparator<>();
+
+    public ZonedHostSorter(List<String> zoneOrder) {
+        this.zoneOrder = zoneOrder != null ? zoneOrder : new ArrayList<>(0);
+    }
+
+    public ZonedHostSorter(String... zoneOrder) {
+        this(Arrays.asList(zoneOrder));
+    }
+
     @Override
     public List<HostSpec> sort(Set<HostSpec> raw) {
-        return null;
+        Map<String, TreeSet<HostSpec>> map = new HashMap<>(zoneOrder.size());
+        raw.forEach(host -> {
+            if (host.zone() != null) {
+                TreeSet<HostSpec> set = map.computeIfAbsent(host.zone(), k -> new TreeSet<>(comparator));
+                set.add(host);
+            }
+        });
+        List<HostSpec> sorted = new LinkedList<>();
+        zoneOrder.forEach(zone -> {
+            TreeSet<HostSpec> set = map.remove(zone.toUpperCase());
+            sorted.addAll(set);
+        });
+        return sorted;
     }
 
 }
---
+++ dal-client/src/test/java/com/ctrip/platform/dal/dao/datasource/cluster/ZonedHostSorterTest.java
@@ -0,0 +1,31 @@
+package com.ctrip.platform.dal.dao.datasource.cluster;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * @author c7ch23en
+ */
+public class ZonedHostSorterTest {
+
+    @Test
+    public void testSort() {
+        ZonedHostSorter sorter = new ZonedHostSorter("a", "b", "c");
+        HostSpec host1 = HostSpec.of("ip1", 0, "c");
+        HostSpec host2 = HostSpec.of("ip2", 0, "b");
+        HostSpec host3 = HostSpec.of("ip3", 0, "b");
+        HostSpec host4 = HostSpec.of("ip4", 0, "a");
+        HostSpec host5 = HostSpec.of("ip5", 0, "x");
+        List<HostSpec> sorted = sorter.sort(new HashSet<>(Arrays.asList(host1, host2, host3, host4, host5)));
+        Assert.assertEquals(4, sorted.size());
+        Assert.assertEquals("ip4", sorted.get(0).host());
+        Assert.assertEquals("ip2", sorted.get(1).host());
+        Assert.assertEquals("ip3", sorted.get(2).host());
+        Assert.assertEquals("ip1", sorted.get(3).host());
+    }
+
+}

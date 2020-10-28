5770e80707b032734ef09dbc3b1a075947738bfd
c7ch23en <chen_cheng@trip.com>
2020-10-28 22:10:33 +0800

    adapt fireman

--- dal-application/src/main/java/com/ctrip/platform/dal/application/fireman/DalForceSwitchDatasourceCallback.java
+++ dal-application/src/main/java/com/ctrip/platform/dal/application/fireman/DalForceSwitchDatasourceCallback.java
@@ -1,30 +1,30 @@
-//package com.ctrip.platform.dal.application.fireman;
-//
-//import com.ctrip.framework.fireman.spi.ForceSwitchDatasourceCallback;
-//
-//public class DalForceSwitchDatasourceCallback implements ForceSwitchDatasourceCallback {
-//    @Override
-//    public void beforeForceSwitch() {
-//
-//    }
-//
-//    @Override
-//    public void afterForceSwitched() {
-//
-//    }
-//
-//    @Override
-//    public void beforeRestore() {
-//
-//    }
-//
-//    @Override
-//    public void afterRestored() {
-//
-//    }
-//
-//    @Override
-//    public int getOrder() {
-//        return HIGHEST_PRECEDENCE;
-//    }
-//}
+package com.ctrip.platform.dal.application.fireman;
+
+import com.ctrip.framework.fireman.spi.ForceSwitchDatasourceCallback;
+
+public class DalForceSwitchDatasourceCallback implements ForceSwitchDatasourceCallback {
+    @Override
+    public void beforeForceSwitch() {
+
+    }
+
+    @Override
+    public void afterForceSwitched() {
+
+    }
+
+    @Override
+    public void beforeRestore() {
+
+    }
+
+    @Override
+    public void afterRestored() {
+
+    }
+
+    @Override
+    public int getOrder() {
+        return HIGHEST_PRECEDENCE;
+    }
+}

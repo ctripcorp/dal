078342af8a9c6f3c961e1728943ba296a80c346d
liuhuanjie <huanjieliu@trip.com>
2020-10-29 16:10:31 +0800

    return db name
    Change-Id: Ic289d163157ae549a48d095fb40db7cc1a8f4aae

--- dal-application/src/main/java/com/ctrip/platform/dal/application/fireman/DalFiremanDependency.java
+++ dal-application/src/main/java/com/ctrip/platform/dal/application/fireman/DalFiremanDependency.java
@@ -27,8 +27,8 @@ public class DalFiremanDependency implements FiremanDependency {
 
     @Override
     public String getDatabaseDomainName() {
-//        return "fxqconfigtestdb";
-        return "fxqconfigtest.mysql.db.fat.qa.nt.ctripcorp.com";
+        return "fxqconfigtestdb";
+//        return "fxqconfigtest.mysql.db.fat.qa.nt.ctripcorp.com";
 //        String domainConnectionString = getDataSource().getSingleDataSource().getDataSourceConfigure().getConnectionString().getDomainConnectionString();
 //        return parser.parse(KEY_NAME, domainConnectionString).getHostName();
     }

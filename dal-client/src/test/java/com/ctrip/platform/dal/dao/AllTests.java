3ac18d2d6ffd31fdfb9d690e16b6e396135c9e14
c7ch23en <chen_cheng@trip.com>
2020-12-25 18:59:19 +0800

    fix tests

--- dal-client/src/test/java/com/ctrip/platform/dal/dao/AllTests.java
+++ dal-client/src/test/java/com/ctrip/platform/dal/dao/AllTests.java
@@ -3,6 +3,7 @@ package com.ctrip.platform.dal.dao;
 
 import com.ctrip.platform.dal.dao.helper.EntityManagerTest.EntityManagerTest;
 import com.ctrip.platform.dal.dao.task._AllTests;
+import com.mysql.jdbc.DalDefaultStatementInterceptorV2Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -23,6 +24,7 @@ import org.junit.runners.Suite.SuiteClasses;
 		com.ctrip.platform.dal.dao.configure._AllTests.class,
         com.ctrip.platform.dal.dao.sharding.idgen._AllTests.class,
         EntityManagerTest.class,
+		DalDefaultStatementInterceptorV2Test.class,
 		/**
 		 * IMPORTANT NOTE! markdown test must be the last one to avoid interfere other test
 		 */

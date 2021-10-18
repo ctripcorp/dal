d9a300a23f4927eb69035b0c28342e7a55801a8b
md_li <md_li@trip.com>
2021-10-18 17:19:39 +0800

    add ClusterConfigXMLParserTest test

--- dal-cluster-client/src/main/java/com/ctrip/framework/dal/cluster/client/config/ClusterConfigValidator.java
+++
@@ -1,9 +0,0 @@
-package com.ctrip.framework.dal.cluster.client.config;
-
-public interface ClusterConfigValidator {
-
-    void validateShardStrategies(String clusterName, String config);
-
-    void validateIdGenerators(String clusterName, String config);
-
-}
--- dal-cluster-client/src/main/java/com/ctrip/framework/dal/cluster/client/sharding/idgen/ClusterIdGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.ctrip.framework.dal.cluster.client.sharding.idgen;
-
-public interface ClusterIdGenerator {
-
-    Number nextId();
-
-}
